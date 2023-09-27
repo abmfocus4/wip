432aa089326206e66427bc413f435c7cef904f01
Meg Alapati <bmalapat@uwaterloo.ca>
2023-09-27 16:00:40 -0400

    Time: 1 ms (90.29%) | Memory: 44.8 MB (16.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,38 +1,65 @@
 class Solution {
-    // Use 2 ref arrs for rows and cols that have all elements 0
+    // Use first row and col as ref arrs
+    // but we don't know if the first row and col need to be 0 as well
+    // these first row and col become 0, if any of the values in the first row and any of the values in the first col are 0 respectively
+    // to check if they should be 0, use two bools
+
     // Time: O(mn)
-    // Space: O(m+n)
+    // Space: O(1)
     public void setZeroes(int[][] matrix) {
-        // sizes used later
+        // to be used later
         int rows = matrix.length;
         int cols = matrix[0].length;
 
-        // reference arrs
-        int[] rowsArr = new int[rows];
-        int[] colsArr = new int[cols];
-        Arrays.fill(rowsArr, -1); // init so that they don't have 0s to begin with
-        Arrays.fill(colsArr, -1);
+        // checking if first row and col need to be set to 0
+        boolean isRow0 = false;
+        boolean isCol0 = false;
 
-        // traverse through each element of matrix
-        // Time: O(mn)
+        // checking if first col needs to be set to 0
         for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
-                // if value in matrix is 0, set corresponding row and col to 0 in arrs
-                if (matrix[i][j] == 0) {
-                    rowsArr[i] = 0;
-                    colsArr[j] = 0;
+            if (matrix[i][0] == 0) { // if any of the values of the first col are 0
+                isCol0 = true;
+            }
+        }
+
+        // checking if first row need to be set to 0
+        for (int i=0; i<cols; i++) {
+            if(matrix[0][i] == 0) { // if any of the values of the row are 0
+                isRow0 = true;
+            }
+        }
+
+        // traverse to remaining elements of matrix
+        for (int i=1; i<rows; i++) { // starts at 1
+            for(int j=1; j<cols; j++) { // starts at 1
+                if (matrix[i][j] == 0) { // if value is 0
+                    matrix[0][j] = 0; // set corresponding first row value to 0
+                    matrix[i][0] = 0; // set corresponding first col value to 0
                 }
             }
         }
 
-        // traverse through each element of matrix
-        // Time: O(mn)
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<cols; j++) {
-                // if ref arrays were set to 0, set values to 0
-                if (rowsArr[i] == 0 || colsArr[j] == 0) {
+        // traverse through remaining elements again to use the first row and col as reference and set the whole remaining matrix to 0 if needed
+        for (int i=1; i<rows; i++) {
+            for(int j=1; j<cols; j++) {
+                // if corresponding first row and col either have 0
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        // setting values of first row 0
+        if (isRow0) {
+            for (int i=0; i<cols; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        // setting values of first col as 0
+        if (isCol0) {
+            for (int i=0; i<rows; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
