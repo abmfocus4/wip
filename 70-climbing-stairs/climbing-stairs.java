a6673c1d8c891c3e6469e71c46f57ad3fdbe7962
Meg Alapati <bmalapat@uwaterloo.ca>
2023-09-27 14:41:16 -0400

    Time: 0 ms (100.00%) | Memory: 38.6 MB (97.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,19 @@
 class Solution {
-    // bottom up dp approach
+    // bottom up dp approach space optimization
     public int climbStairs(int n) {
         // base case
         if (n == 0 || n == 1) {
             return 1;
         }
 
-        // nth step is needed, array need to be n+1
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 1; // store base value
-        
+        int prev = 1;
+        int cur = 1;
         for (int i = 2; i <= n; i++) { // step calculation after base case
-            dp[i] = dp[i-1] + dp[i-2];
+            int temp = cur;
+            cur = prev + cur; // sum last value with current value
+            prev = temp; // assign to old current value
         }
 
-        return dp[n];
+        return cur;
     }
 }
