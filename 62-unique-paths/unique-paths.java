00d514e0bc0e1b3376fcfeec9dffc8e4f6abe00d
Meg Alapati <bmalapat@uwaterloo.ca>
2023-09-27 13:38:47 -0400

    Time: 0 ms (100.00%) | Memory: 38.9 MB (85.77%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,42 +1,22 @@
-// Permutations problem:
-// Given in question, 3x7 matrix, robot needs to take 2+6 = 8 steps with 2 down and 6 right in any order. That is nothing but a permutation problem.
-// We have to tell the total number of permutations of the above given word. So, decrease both m & n by 1 and apply following formula:-
-// Total permutations = (m+n)! / (m! * n!)
-// But as m and n increase size of (m+n)! will also increase
-// ref : https://www.youtube.com/watch?v=Z-uMFv_-w2s
 class Solution {
-    // Recursive solution + top down dp (using cache)
-    // Time: exponential for recursive, Space: O(1)
-    // Time: O(mn) Space: O(mn)
-    int m;
-    int n;
-    Integer[][] cache;
+    // dp: bottom up 
+    // Time/Space: O(mn)
     public int uniquePaths(int m, int n) {
-        this.m = m;
-        this.n = n;
-        cache = new Integer[m][n];
-        // returns unique row for the particular row and col
-        return helper(0,0);
-    }
+        int[][] cache = new int[m][n];
+        // fill all row boundaries to 1 because only 1 direction can go (up or down you go out of grid)
+        for(int[] row : cache) {
+            Arrays.fill(row, 1);
+        }
 
-    private int helper(int row, int col) {
-        // go over the boundary
-        if (row >= m || col >= n) return 0;
+        // calculate grid values from target to up and down to reach 0,0
+        for (int i = m-2; i >= 0; i--) {
+            for (int j = n-2; j >= 0; j--) {
+                int up = cache[i+1][j];
+                int left = cache[i][j+1];
+                cache[i][j] = up + left;
+            }
+        }
 
-        // reached last box
-        if (row == m-1 || col == n-1) return 1;
-
-        // if value was previously computed, return value
-        if (cache[row][col] != null) return cache[row][col];
-
-        // calculate routes if down box is taken
-        int down = helper(row+1, col);
-        // calculate routes if right box is taken
-        int right = helper(row, col+1);
-
-        // cache populate computed values
-        cache[row][col] = down + right;
-
-        return cache[row][col];
+        return cache[0][0];
     }
 }
