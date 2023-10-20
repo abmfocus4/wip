b5ddab658840beb2c06f63abb41548a2e52097da
Meg Alapati <bmalapat@uwaterloo.ca>
2023-10-19 20:17:26 -0400

    Time: 3 ms (83.78%) | Memory: 40.6 MB (91.62%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,29 +1,19 @@
 class Solution {
-    String s;
-    List<String> wordDict;
-    int[] memo; // stands for memoization
-    private boolean dp(int i) { // calculate dp states
-        if (i < 0) return true; // boundary checking/ edge case
-        if (memo[i] != -1) return memo[i] == 1; // don't calculate case more than once
-
-        for (String word : wordDict) { // check all words in dict
-            if (i < word.length() - 1) continue; // boundary checking
-
-            // actual dp case
-            if (s.substring(i - word.length() + 1, i + 1).equals(word) && dp(i - word.length())) {
-                memo[i] = 1;
-                return true;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            for (String word : wordDict) {
+                if (i < word.length() - 1) continue;
+                // check before first index of word in s, the string can word break
+                // or if word is the first word of word break (so that dp of [i-word.length] does not become neg)
+                if (i - word.length() + 1 == 0|| dp[i - word.length()]) {
+                    if (s.substring(i - word.length() + 1, i + 1).equals(word)) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
             }
         }
-        // negative state
-        memo[i] = 0;
-        return false;
-    }
-    public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1); // init with starting value (different from 1 and 0)
-        return dp(s.length() - 1); // we want to check if last index passed uses all words in dict
+        return dp[s.length()-1];
     }
 }
