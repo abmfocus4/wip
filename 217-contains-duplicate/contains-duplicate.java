b11bcf4e82f086accb5c929e57df6c149c78f4f5
Meg Alapati <bmalapat@uwaterloo.ca>
2023-10-22 00:27:03 -0400

    Time: 7 ms (97.74%) | Memory: 55.9 MB (40.83%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,18 @@
+// Ref : https://leetcode.com/problems/contains-duplicate/solutions/2459020/very-easy-100-fully-explained-c-java-python-javascript-python3-creating-set/?envType=list&envId=pxw54vnt
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // single iteration of nums
+        // add each to hash set and check if it contains
+        // if it contains return true
+        // else nothing
+        // after loop exits return false
+        if (nums == null || nums.length == 0) return false;
+        Set<Integer> set = new HashSet();
+
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        
+        return false;
+    }
+}
