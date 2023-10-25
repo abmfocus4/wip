b215b0391297d50973efa763c749ec9353bbd131
Meg Alapati <bmalapat@uwaterloo.ca>
2023-10-25 15:36:26 -0400

    Time: 2 ms (96.72%) | Memory: 42.9 MB (71.58%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -21,28 +21,23 @@
  // Time: O(m*m)
 
  // Space: O(height of first tree) == O(m) worst case and O(logm) balanced tree case
+
+ // Recursive
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        // take queue- add root to queue
-        // for each node in root = check if is same subtree
-        // add left and right to queue if not null
-        // repeat until queue is empty
+        // if node is null return false
+        // check if root matches the subRoot, if yes, return true
+        // else check if root's left matches subRoot or root's right matches subRoot
 
         // if root is null and subroot is null return true
         // if root is null or subroot is null return false
         // if value of root is not same, then return false
         // else compare left nodes and right nodes of each tree respectively
 
-        Queue<TreeNode> q = new LinkedList();
-        q.add(root);
-        while (q.isEmpty() != true) {
-            TreeNode node = q.poll();
-            if (isSameTree(node, subRoot)) return true;
-            if (node.left != null) q.add(node.left);
-            if (node.right != null) q.add(node.right);
-        }
-
-        return false;
+        if (root == null) return false; // to make sure that when we access left and right of root we don't get error
+        if (isSameTree(root, subRoot)) return true;
+        else return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
     }
 
     private boolean isSameTree(TreeNode node, TreeNode subRoot) {
