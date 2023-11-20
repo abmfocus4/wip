19cd1c5a36b23692975fc84468a64c62311bf599
Meg Alapati <bmalapat@uwaterloo.ca>
2023-11-20 11:29:46 -0500

    Time: 1 ms (100.00%) | Memory: 52.8 MB (50.37%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -3,24 +3,27 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int N = nums.length;
 
-        // create left products array
-        int[] left_products = new int[N];
-        left_products[0] = 1;
-        for (int i = 1; i < N; i++) {
-            left_products[i] = left_products[i-1] * nums[i-1];
-        }
-
-        // create right products array
-        int[] right_products = new int[N];
-        right_products[N-1] = 1;
-        for (int i = N-2; i >= 0; i--) {
-            right_products[i] = right_products[i+1] * nums[i+1];
-        }
-
-        // create output array
+        // replace left products array with output_arr (reuse output_arr)
         int[] output_arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            output_arr[i] = left_products[i] * right_products[i];
+        output_arr[0] = 1;
+        for (int i = 1; i < N; i++) {
+            output_arr[i] = output_arr[i-1] * nums[i-1];
+        }
+
+        // ===============OMIT===============
+        // // create right products array
+        // int[] right_products = new int[N];
+        // right_products[N-1] = 1;
+        // for (int i = N-2; i >= 0; i--) {
+        //     right_products[i] = right_products[i+1] * nums[i+1];
+        // }
+
+        // store right products in temp var
+        // compute output
+        int R = 1;
+        for (int i = N-1; i >= 0; i--) {
+            output_arr[i] = output_arr[i] * R;
+            R = R * nums[i]; // compute right for next iteration, that's why you don't need to use i+1 elem of nums
         }
 
         return output_arr;
