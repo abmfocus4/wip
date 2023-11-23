1ae7b1abb6a867045896886c75558de8be62d0a0
Meg Alapati <bmalapat@uwaterloo.ca>
2023-11-23 17:56:41 -0500

    Time: 14 ms (86.36%) | Memory: 43.4 MB (42.30%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,38 +5,17 @@
 
 // why not greedy? - https://leetcode.com/problems/coin-change/description/comments/2008918
 class Solution {
-    int[] coins;
-    Integer[] cache;
     public int coinChange(int[] coins, int amount) {
-        this.coins = coins;
-        this.cache = new Integer[amount + 1]; // starting from 0 = 0,1,2,3,...,n = (n+1)
-        return helper(amount); // returns min coins to get this amt
-    }
-
-    private int helper(int amount) {
-        if (amount == 0) return 0;
-        if (amount < 0) return -1;
-
-        if (cache[amount] != null) return cache[amount];
-
-        int minCoinChange = Integer.MAX_VALUE;
-        
-        for (int num : coins) {
-            int curMinCoinChange = helper(amount - num); // like dp[amt - num]
-            if (curMinCoinChange != -1) {
-                minCoinChange = Math.min(minCoinChange, curMinCoinChange);
+        int[] cache = new int[amount + 1];
+        Arrays.fill(cache, amount + 1);
+        cache[0] = 0;
+        for (int i = 1; i < amount + 1; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    cache[i] = Math.min(cache[i], cache[i-coin] + 1);
+                }
             }
         }
-
-        // minCoinChange rn is min of dp[amt - num] given some num in coins
-
-        if (minCoinChange == Integer.MAX_VALUE) {
-            minCoinChange = -1;
-        } else {
-            minCoinChange += 1; // only one coin is added to dp[amt - num] == total path
-        }
-
-        cache[amount] = minCoinChange;
-        return cache[amount];
+        return cache[amount] == amount + 1 ? -1 : cache[amount];
     }
 }
