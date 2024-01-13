e8c876be7e43215048141328771bdb5ee4772271
Meg Alapati <bmalapat@uwaterloo.ca>
2024-01-13 12:43:31 -0500

    Time: 4 ms (97.88%) | Memory: 46.1 MB (6.69%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -1,82 +1,74 @@
-// Ref : https://www.youtube.com/watch?v=ZQp1oGp1y6s
-// BFS
 class Solution {
     int[][] heights;
+    int M;
+    int N;
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        this.heights = heights; // init heights grid
+        this.heights = heights;
         List<List<Integer>> res = new LinkedList();
 
-        int M = heights.length; // row size
-        int N = heights[0].length; // col size
+        this.M = heights.length;
+        this.N = heights[0].length;
 
-        Queue<int[]> q = new LinkedList(); // q of coordinate int[]
-        
-        // bfs over pacific ocean
-        addRow(0, q, N);
-        addCol(0, q, M);
-        boolean[][] pacificTable = bfs(q, M, N);
+        // dfs - reachable to pacific ocean
+        boolean[][] pacificTable = new boolean[M][N];
+        iterateRow(0, pacificTable);
+        iterateCol(0, pacificTable);
 
-        // bfs over atlantic ocean
-        addRow(M-1, q, N);
-        addCol(N-1, q, M);
-        boolean[][] atlanticTable = bfs(q, M, N);
+        // dfs - reachable to atlantic ocean
+        boolean[][] atlanticTable = new boolean[M][N];
+        iterateRow(M-1, atlanticTable);
+        iterateCol(N-1, atlanticTable);
 
-        // check cells that can access both pacific and atlantic ocean
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 if (atlanticTable[i][j] == true && pacificTable[i][j] == true) {
-                    res.add(Arrays.asList(new Integer[]{i, j}));
+                    res.add(Arrays.asList(new Integer[] { i , j}));
                 }
             }
         }
         return res;
     }
 
-    private void addRow(int row, Queue<int[]> q, int totalCols) {
+    private void iterateRow(int row, boolean[][] visited) {
+        int totalCols = N;
         for (int i = 0; i < totalCols; i++) {
             int col = i;
-            int[] coor = {row, col};
-            q.add(coor);
+            dfs(row, col, visited);
         }
     }
 
-    private void addCol(int col, Queue<int[]> q, int totalRows) {
+    private void iterateCol(int col, boolean[][] visited) {
+        int totalRows = M;
         for (int i = 0; i < totalRows; i++) {
             int row = i;
-            int[] coor = {row, col};
-            q.add(coor);
+            dfs(row, col, visited);
         }
     }
 
-    private boolean[][] bfs(Queue<int[]> q, int M, int N) {
-        boolean[][] visited = new boolean[M][N];
+    private void dfs(int row, int col, boolean[][] visited) {
+        if (visited[row][col] == true) return;
         
-        while (q.isEmpty() == false) {
-            int[] first = q.poll();
-            int row = first[0];
-            int col = first[1];
-            if (visited[row][col] == true) {
-                continue;
-            }
-            visited[row][col] = true;
-            int curCellVal = heights[row][col];
-            
-            // Top
-            if (row - 1 >= 0 && curCellVal <= heights[row-1][col])
-                q.add(new int[] {row-1, col});
+        visited[row][col] = true;
+        int curCellVal = heights[row][col];
 
-            // Left
-            if (col - 1 >= 0 && curCellVal <= heights[row][col-1])
-                q.add(new int[] {row, col-1});
-
-            // Right
-            if (col + 1 < N && curCellVal <= heights[row][col+1])
-                q.add(new int[] {row, col+1});
-
-            // Down
-            if (row + 1 < M && curCellVal <= heights[row+1][col])
-                q.add(new int[] {row+1, col});
+        // Top
+        if (row-1 >= 0 && curCellVal <= heights[row-1][col]) {
+            dfs(row-1, col, visited);
         }
-        return visited;
+
+        // Down
+        if (row+1 < M && curCellVal <= heights[row+1][col]) {
+            dfs(row+1, col, visited);
+        }
+
+        // Left
+        if (col-1 >= 0 && curCellVal <= heights[row][col-1]) {
+            dfs(row, col-1, visited);
+        }
+
+        // Right
+        if (col+1 < N && curCellVal <= heights[row][col+1]) {
+            dfs(row, col+1, visited);
+        }
     }
 }
