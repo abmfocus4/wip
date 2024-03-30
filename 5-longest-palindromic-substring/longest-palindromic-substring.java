60dce2e0a715f1a032081f6f4f148669de861b10
Meg Alapati <bmalapat@uwaterloo.ca>
2024-03-30 18:43:37 -0400

    Time: 114 ms (43.77%) | Memory: 46.8 MB (8.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,40 @@
 class Solution {
     public String longestPalindrome(String s) {
-        // for each substring (in descending)
-        // if is palindrome : return substr
-        
-        for (int len = s.length(); len > 0; len--) {
-            for (int start = 0; start <= s.length() - len; start++) {
-                if (isPalindrome(s, start, start+len)) {
-                    return s.substring(start, start+len);
+        // use 2d array with row = start and column = end bounds of palindrome
+        // base cases:
+        // odd length palindrome reflected around i
+        // even length palindromes reflected around i+1 (non char center)
+        // dp : s[i] == s[j] && dp[i+1][j-1]
+
+        int[] bounds = new int[] {0,0};
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n-1; i++) {
+            if (s.charAt(i) == s.charAt(i+1))
+            {
+                dp[i][i+1] = true;
+                bounds[0] = i;
+                bounds[1] = i+1;
+            }
+        }
+
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i+diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i+1][j-1]) {
+                    dp[i][j] = true;
+                    bounds[0] = i;
+                    bounds[1] = j;
                 }
             }
         }
-        return "";
-    }
 
-    public boolean isPalindrome(String s, int left, int right) {
-        int i = left;
-        int j = right - 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        return s.substring(bounds[0], bounds[1]+1);
+
     }
 }
