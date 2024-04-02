63fc003cb141dbe17c7071f89f18050d5cc87bb5
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-02 16:35:14 -0400

    Time: 0 ms (100.00%) | Memory: 40.5 MB (34.95%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,22 +1,19 @@
 class Solution {
-    // dp: bottom up 
-    // Time/Space: O(mn)
     public int uniquePaths(int m, int n) {
         int[][] cache = new int[m][n];
-        // fill all row boundaries to 1 because only 1 direction can go (up or down you go out of grid)
-        for(int[] row : cache) {
-            Arrays.fill(row, 1);
+        for (int[] arr : cache) {
+            Arrays.fill(arr, -1);
         }
+        return uniquePathsFrom(0, 0, cache, m, n);
+    }
 
-        // calculate grid values from target to up and down to reach 0,0
-        for (int i = m-2; i >= 0; i--) {
-            for (int j = n-2; j >= 0; j--) {
-                int up = cache[i+1][j];
-                int left = cache[i][j+1];
-                cache[i][j] = up + left;
-            }
-        }
-
-        return cache[0][0];
+    public int uniquePathsFrom(int row, int col, int[][] cache, int m, int n) {
+        if (row > m - 1 || col > n - 1) return 0;
+        if (row == m-1 && col == n-1) return 1;
+        if (cache[row][col] != -1) return cache[row][col];
+        int bottom = uniquePathsFrom(row+1, col, cache, m, n);
+        int right = uniquePathsFrom(row, col+1, cache, m, n);
+        cache[row][col] = bottom+right;
+        return cache[row][col];
     }
 }
