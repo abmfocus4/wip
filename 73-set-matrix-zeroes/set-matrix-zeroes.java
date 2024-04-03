691b892b65ff5cb9a4544210f140054f866ae179
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-03 15:13:26 -0400

    Time: 1 ms (73.58%) | Memory: 45.2 MB (93.53%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,65 +1,35 @@
 class Solution {
-    // Use first row and col as ref arrs
-    // but we don't know if the first row and col need to be 0 as well
-    // these first row and col become 0, if any of the values in the first row and any of the values in the first col are 0 respectively
-    // to check if they should be 0, use two bools
-
-    // Time: O(mn)
-    // Space: O(1)
     public void setZeroes(int[][] matrix) {
-        // to be used later
+
+        // make a copy of matrix
+        // traverse orig matrix and set 0s in copy matrix
+        // copy matrix to orig matrix data transfer
         int rows = matrix.length;
         int cols = matrix[0].length;
-
-        // checking if first row and col need to be set to 0
-        boolean isRow0 = false;
-        boolean isCol0 = false;
-
-        // checking if first col needs to be set to 0
-        for (int i=0; i<rows; i++) {
-            if (matrix[i][0] == 0) { // if any of the values of the first col are 0
-                isCol0 = true;
+        int[][] copy = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                copy[i][j] = matrix[i][j];
             }
         }
 
-        // checking if first row need to be set to 0
-        for (int i=0; i<cols; i++) {
-            if(matrix[0][i] == 0) { // if any of the values of the row are 0
-                isRow0 = true;
-            }
-        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    for (int r = 0; r < rows; r++) {
+                        copy[r][j] = 0;
+                    }
 
-        // traverse to remaining elements of matrix
-        for (int i=1; i<rows; i++) { // starts at 1
-            for(int j=1; j<cols; j++) { // starts at 1
-                if (matrix[i][j] == 0) { // if value is 0
-                    matrix[0][j] = 0; // set corresponding first row value to 0
-                    matrix[i][0] = 0; // set corresponding first col value to 0
+                    for (int c = 0; c < cols; c++) {
+                        copy[i][c] = 0;
+                    }
                 }
             }
         }
 
-        // traverse through remaining elements again to use the first row and col as reference and set the whole remaining matrix to 0 if needed
-        for (int i=1; i<rows; i++) {
-            for(int j=1; j<cols; j++) {
-                // if corresponding first row and col either have 0
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // setting values of first row 0
-        if (isRow0) {
-            for (int i=0; i<cols; i++) {
-                matrix[0][i] = 0;
-            }
-        }
-
-        // setting values of first col as 0
-        if (isCol0) {
-            for (int i=0; i<rows; i++) {
-                matrix[i][0] = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = copy[i][j];
             }
         }
     }
