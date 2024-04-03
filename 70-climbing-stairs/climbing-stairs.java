dc3fcf78429bc0ec0ced9a8728bb8a85d25e2dcd
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-03 14:07:43 -0400

    Time: 0 ms (100.00%) | Memory: 39.6 MB (98.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,21 +1,15 @@
 class Solution {
-    // top down approach
-    // parent stair = child stair tells from many ways to climb to top
+    // bottom up approach
+    // already at step n - 1, how to compute step 0 onwards distinct ways
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return climbStairsFrom(n, dp, 0);
-    }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 1);
 
-    public int climbStairsFrom(int n, int[] dp, int stair) {
-        if (stair > n) return 0;
-        if (dp[stair] != -1) return dp[stair];
-        if (stair == n) return 1;
-
-        int oneStep = climbStairsFrom(n, dp, stair + 1);
-        int twoStep = climbStairsFrom(n, dp, stair + 2);
-
-        dp[stair] = oneStep + twoStep;
-        return dp[stair]; 
+        for (int i = n-2; i >= 0; i--) {
+            int oneStep = dp[i+1];
+            int twoStep = dp[i+2];
+            dp[i] = oneStep + twoStep;
+        }
+        return dp[0];
     }
 }
