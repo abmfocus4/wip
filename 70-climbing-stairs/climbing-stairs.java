63115aacd5a5384ee4524e9dfec39d8a979fa00b
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-03 14:02:59 -0400

    Time: 0 ms (100.00%) | Memory: 40 MB (77.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,21 @@
 class Solution {
-    // bottom up dp approach space optimization
+    // top down approach
+    // parent stair = child stair tells from many ways to climb to top
     public int climbStairs(int n) {
-        // base case
-        if (n == 0 || n == 1) {
-            return 1;
-        }
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return climbStairsFrom(n, dp, 0);
+    }
 
-        int prev = 1;
-        int cur = 1;
-        for (int i = 2; i <= n; i++) { // step calculation after base case
-            int temp = cur;
-            cur = prev + cur; // sum last value with current value
-            prev = temp; // assign to old current value
-        }
+    public int climbStairsFrom(int n, int[] dp, int stair) {
+        if (stair > n) return 0;
+        if (dp[stair] != -1) return dp[stair];
+        if (stair == n) return 1;
 
-        return cur;
+        int oneStep = climbStairsFrom(n, dp, stair + 1);
+        int twoStep = climbStairsFrom(n, dp, stair + 2);
+
+        dp[stair] = oneStep + twoStep;
+        return dp[stair]; 
     }
 }
