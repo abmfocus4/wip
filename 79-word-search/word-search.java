da2223ce3e23374cc7dae1e3cd93446e56d200b8
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-04 14:37:57 -0400

    Time: 16 ms (97.88%) | Memory: 41.6 MB (49.01%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,55 +1,55 @@
 class Solution {
-    // Time: O(N^2)
-    boolean visited[][]; // avoid repeatedly searching for the same element (using duplicate indexes)
     public boolean exist(char[][] board, String word) {
+        // find the first letter and then find the word from there
+        // use a 2d visited arr to make sure you are not revisiting already seen cells
+        // frequency check opt
+        // reverse word opt
+
         int rows = board.length;
         int cols = board[0].length;
 
-        int[] boardf = new int[128]; // frequency board
+        // frequency check
+        int[] boardf = new int[128];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                boardf[board[i][j]]++; // using ascii directly
+                boardf[board[i][j]]++;
             }
         }
 
-        char[] word_arr = word.toCharArray();
-
-        for (char w : word_arr) {
-            if (boardf[w]-- < 0) return false; // frequency doesn't match exit early
+        for (char c : word.toCharArray()) {
+            if (boardf[c]-- < 0) return false;
         }
 
-        // reverse word if number of cells with starting letter is more than number of cells with ending letter (reverse or not result is same)
-        if (boardf[word_arr[0]] > boardf[word_arr[word.length() - 1]]) {
+        // reverse opt
+        if (boardf[word.charAt(0)] > boardf[word.charAt(word.length() - 1)]) {
             word = reverse(word);
         }
 
-        visited = new boolean[rows][cols];
-
+        boolean[][] visited = new boolean[rows][cols];
+        for (boolean[] arr : visited) {
+            Arrays.fill(arr, false);
+        }
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (board[i][j] == word.charAt(0) && wordSearch(i, j, 0, board, word)) {
+                if (board[i][j] == word.charAt(0) && wordSearch(i, j, 0, visited, board, word)) {
                     return true;
                 }
             }
         }
+
         return false;
+        
     }
-    private boolean wordSearch(int i, int j, int index, char[][] board, String word) {
-        // base cases
-        if (index == word.length())  return true;
 
-        if (i >= board.length || j >= board[0].length || i < 0 || j < 0 || word.charAt(index) != board[i][j] || visited[i][j]) return false;
-
-        // processing new index
+    public boolean wordSearch(int i, int j, int index, boolean[][] visited, char[][] board, String word) {
+        if (index == word.length()) return true;
+        if (i >= board.length || j >= board[0].length || i < 0 || j < 0 || visited[i][j] || word.charAt(index) != board[i][j]) return false;
         visited[i][j] = true;
-        if (
-            wordSearch(i, j-1, index+1, board, word) ||
-            wordSearch(i, j+1, index+1, board, word) ||
-            wordSearch(i-1, j, index+1, board, word) ||
-            wordSearch(i+1, j, index+1, board, word)
-        ) return true;
-
-        // if you did not hit any conditions above
+        if (wordSearch(i + 1, j, index + 1, visited, board, word) || wordSearch(i - 1, j, index + 1, visited, board, word)
+                || wordSearch(i, j + 1, index + 1, visited, board, word)
+                || wordSearch(i, j - 1, index + 1, visited, board, word)) {
+            return true;
+        }
         visited[i][j] = false;
         return false;
     }
@@ -57,11 +57,13 @@ class Solution {
     public String reverse(String word) {
         int N = word.length();
         char[] word_arr = word.toCharArray();
-        for (int i = 0; i < N/2; i++) {
+
+        for (int i = 0; i < N/2; i ++) {
             char temp = word_arr[i];
-            word_arr[i] = word_arr[N - i - 1];
+            word_arr[i] = word_arr[N-i-1];
             word_arr[N-i-1] = temp;
         }
+
         return String.valueOf(word_arr);
     }
 }
