298c1b9c3ab35c15c294e8bf5552ce9b9445e272
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-04 15:47:28 -0400

    Time: 1 ms (80.60%) | Memory: 41.6 MB (67.15%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.length() == 0 || s.charAt(0) == '0') return 0;
+
+        if (s.length() == 1) return 1;
+
+        int count1 = 1; 
+        int count2 = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int d = s.charAt(i) - '0';
+            int dd = d + (s.charAt(i-1) - '0')*10;
+            int count = 0;
+            if (d > 0) count += count1;
+            if (10 <= dd && dd <= 26) count += count2;
+            count2 = count1;
+            count1 = count;
+        }
+
+        return count1;
+    }
+}
