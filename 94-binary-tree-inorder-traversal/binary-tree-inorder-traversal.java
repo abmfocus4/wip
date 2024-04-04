05f08470dd2b948c75e54d37a12a6ea84e6bfc56
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-04 16:01:42 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (39.90%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,36 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
-
 // Left -> Root -> Right
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList();
-        if (root == null) return list;
+        if (root == null)
+            return list;
         Stack<TreeNode> stack = new Stack();
-
-        while (root != null || !stack.isEmpty()) { // be careful - use || and not &&
-            while (root != null) { // left traversal
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
             root = stack.pop();
-            list.add(root.val); // left or root
-            root = root.right; // traverse right tree
+            list.add(root.val);
+            root = root.right;
         }
-
         return list;
-}
+    }
 }
