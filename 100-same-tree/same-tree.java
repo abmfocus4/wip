e4ce5b28997bf49f095529009ca051f26ec5a9f1
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-05 11:30:43 -0400

    Time: 0 ms (100.00%) | Memory: 41 MB (45.39%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -15,13 +15,27 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        // check null corner cases
-        // check if root val is equal
-        // recursive check left and right subtrees
-        if (p == null && q == null) return true;
-        if (p == null || q == null) return false;
-        if (p.val != q.val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        // use queue to sequentially compare corresponding nodes in each subtree
+        // first add the roots of the trees
+        // while the queue is not empty
+        // complete equality checks
+        // add left and right nodes to queue
+        // return true once the loop is exited
 
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(p);
+        queue.add(q);
+        while(!queue.isEmpty()) {
+            TreeNode a = queue.poll();
+            TreeNode b = queue.poll();
+            if (a == null && b == null) continue;
+            else if (a == null || b == null || a.val != b.val) return false;
+            queue.add(a.left);
+            queue.add(b.left);
+            queue.add(a.right);
+            queue.add(b.right);
+        }
+
+        return true;
     }
 }
