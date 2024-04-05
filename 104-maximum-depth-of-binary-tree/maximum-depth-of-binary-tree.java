a6ef93274e3a43e9c0936575909d9b82865a5856
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-05 11:52:28 -0400

    Time: 1 ms (10.50%) | Memory: 43 MB (9.21%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,9 +15,21 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
+        //maxDepth == maxLevels
+        // do a BFS (level order traversal) using queue to find the max depth
+        int maxDepth = 0;
         if (root == null) return 0;
-        int maxDepthLeft = maxDepth(root.left);
-        int maxDepthRight = maxDepth(root.right);
-        return Math.max(maxDepthLeft, maxDepthRight) + 1;
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode cur = q.poll();
+                if (cur.left != null) q.add(cur.left);
+                if (cur.right != null) q.add(cur.right);
+            }
+            maxDepth++;
+        }
+        return maxDepth;
     }
 }
