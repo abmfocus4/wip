24fa81a095818428f7e1520a71bbc3c015ef5c7a
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-05 14:34:23 -0400

    Time: 1 ms (94.23%) | Memory: 43.9 MB (82.05%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -20,26 +20,27 @@ class Solution {
         // inorder arr for finding left and right subtree
         // leaf nodes for null can be found if boundary checks
 
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
+        // optimization: use hash table to make algorith linear time
+        // question says no duplicates so no checking required = O(n^2) time and O(n) [recursion] space to O(n) time and space
+        HashMap<Integer, Integer> inMap = new HashMap();
+        for (int i = 0; i < inorder.length; i++) {
+            inMap.put(inorder[i], i);
+        }
+
+        return helper(0, 0, inorder.length - 1, preorder, inorder, inMap); // pass InMap
         
     }
 
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder, HashMap<Integer, Integer> inMap) {
         if (preStart > preorder.length - 1 || inStart > inEnd) return null; //leaf nodes
         TreeNode root = new TreeNode(preorder[preStart]);
 
-        int inRootIndex = 0;
-        for (int i = inStart; i <= inEnd; i++) {
-            if (root.val == inorder[i]) {
-                inRootIndex = i;
-                break;
-            }
-        }
+        int inRootIndex = inMap.get(root.val); // get root index from inMap
 
         int numLeftNodes = inRootIndex - inStart;
 
-        root.left = helper(preStart + 1, inStart, inRootIndex - 1, preorder, inorder);
-        root.right = helper(preStart + 1 + numLeftNodes, inRootIndex + 1, inEnd, preorder, inorder);
+        root.left = helper(preStart + 1, inStart, inRootIndex - 1, preorder, inorder, inMap); // pass InMap
+        root.right = helper(preStart + 1 + numLeftNodes, inRootIndex + 1, inEnd, preorder, inorder, inMap); // passInMap
         return root;
     }
 }
