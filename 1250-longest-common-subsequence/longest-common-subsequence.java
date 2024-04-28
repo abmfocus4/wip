d91f1bafb9d459b1346fc5e1b3fe48fa31b6d82c
Meg Alapati <bmalapat@uwaterloo.ca>
2024-04-28 10:54:51 -0400

    Time: 17 ms (90.54%) | Memory: 41.3 MB (98.39%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,23 +1,34 @@
 // https://www.youtube.com/watch?v=e9tUPwZZSBI&ab_channel=NikhilLohia
+// improvement: https://leetcode.com/problems/longest-common-subsequence/solutions/351689/java-python-3-two-dp-codes-of-o-mn-o-min-m-n-spaces-w-picture-and-analysis
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-        for (int[] row : dp) {
-            Arrays.fill(row, 0);
-        }
+        // int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        // for (int[] row : dp) {
+        //     Arrays.fill(row, 0);
+        // }
         int M = text1.length();
         int N = text2.length();
 
+        int prevRow;
+        int prevRowCol;
+
+        int[] dp = new int[N+1];
+        Arrays.fill(dp, 0);
+
         for (int i = 1; i <= M; i++) {
+            prevRow = 0;
+            prevRowCol = 0;
             for (int j = 1; j <= N; j++) {
+                prevRowCol = prevRow;
+                prevRow = dp[j];
                 if (text1.charAt(i-1) == text2.charAt(j-1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    dp[j] = 1 + prevRowCol;
                 } else {
-                    dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                    dp[j] = Math.max(prevRow, dp[j-1]);
                 }
             }
         }
 
-        return dp[M][N];
+        return dp[N];
     }
 }
