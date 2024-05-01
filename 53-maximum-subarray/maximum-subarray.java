bb1d69cf6b50f09d99f0e85dfdc7dd188ce6d8ff
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-01 14:22:14 -0400

    Time: 16 ms (7.49%) | Memory: 57.1 MB (27.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,11 +1,41 @@
+// https://leetcode.com/problems/maximum-subarray/solutions/1595186/java-kadane-divide-and-conquer-dp
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int curSum = maxSum;
-        for (int i = 1; i < nums.length; i++) {
-            curSum = Math.max(nums[i], nums[i] + curSum);
-            maxSum = Math.max(maxSum, curSum);
+    
+        return helper(nums,0,nums.length-1);
+    }
+    
+    public int helper(int nums[],int i,int j){
+
+        if(i==j){
+            return nums[i];
         }
-        return maxSum;
+                          
+        int mid  =  (i+j)/2;
+        int sum = 0,leftMaxSUM = Integer.MIN_VALUE;
+        
+        for(int l =  mid;l>=i;l--){
+            sum+=nums[l];
+            if(sum>leftMaxSUM){
+                leftMaxSUM =  sum;
+            }                                    
+        }
+        
+     int rightMaxSUM = Integer.MIN_VALUE;
+        sum = 0;    // reset sum to 0
+        for (int l = mid + 1; l <=j; l++)
+        {
+            sum += nums[l];
+            if (sum > rightMaxSUM ) {
+                rightMaxSUM = sum;
+            }
+        }
+        
+       int maxLeftRight = Math.max(helper(nums, i, mid),
+                                    helper(nums, mid + 1, j ));
+        return Math.max(maxLeftRight, leftMaxSUM + rightMaxSUM );
+        
+        
+    
     }
 }
