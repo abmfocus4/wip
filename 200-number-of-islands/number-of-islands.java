72c6023e4c7a5b25511b7a9c3914ea9bda145841
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-02 23:40:37 -0400

    Time: 5 ms (26.18%) | Memory: 48.6 MB (96.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -8,16 +8,16 @@ class Solution {
         int n = grid[0].length;
 
         int count = 0;
-        boolean[][] visited = new boolean[m][n];
+        // boolean[][] visited = new boolean[m][n];
         Queue<int[]> q = new LinkedList<>();
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
+                if (grid[i][j] == '1') {
                     count++;
                     q.offer(new int[]{i, j});
-                    visited[i][j] = true;
-                    BFS(i, j, grid, visited, q);
+                    // visited[i][j] = true;
+                    BFS(i, j, grid, q);
                 }
             }
         }
@@ -25,16 +25,16 @@ class Solution {
     }
 
     int[][] dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-    private void BFS(int i, int j, char[][] grid, boolean[][] visited, Queue<int[]> q) {
+    private void BFS(int i, int j, char[][] grid, Queue<int[]> q) {
         while(!q.isEmpty()) {
             int[] curr = q.poll();
             for (int[] dir : dirs) {
                 int x = curr[0] + dir[0], y = curr[1] + dir[1];
-                if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || visited[x][y] || grid[x][y] == '0') {
+                if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length|| grid[x][y] == '0') {
                     continue;
                 }
+                grid[x][y] = '0';
                 q.offer(new int[] {x, y});
-                visited[x][y] = true;
             }
         }
     }
