0a29e3574001a592cc749ff3badfacc44b76cfb2
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-04 19:58:03 -0400

    Time: 1 ms (95.39%) | Memory: 45.3 MB (61.17%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/can-place-flowers/solutions/3317843/java-c-simple-solution-easy-to-understand
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            // only way you can make a 0 to 1 is if, 
+            // prev value is 0 and 
+            // next value is 0
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
