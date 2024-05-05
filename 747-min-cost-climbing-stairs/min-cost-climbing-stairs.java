a134f4166cd05e80c0aa02ef2969715518fd3574
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-05 12:51:50 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (89.69%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,12 @@
+// greedy: not related to dp
+// if we can modify cost
+class Solution {
+    // if we can modify the staircase cost arr
+    public int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
+    }
+}
