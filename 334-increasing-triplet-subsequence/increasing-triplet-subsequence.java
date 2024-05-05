5877dd995ec936ef73820deb24eddde238e3edb4
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-05 19:52:21 -0400

    Time: 2 ms (94.07%) | Memory: 134.1 MB (24.76%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/increasing-triplet-subsequence/solutions/79004/concise-java-solution-with-comments
+class Solution {
+       public boolean increasingTriplet(int[] nums) {
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) { 
+                small = n; 
+            } else if (n <= big) { 
+                big = n; 
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
