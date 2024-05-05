1bb6b62fd23ef4cf49c0a2e8e9ef319f6f27337a
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-05 11:59:24 -0400

    Time: 0 ms (100.00%) | Memory: 45.1 MB (63.53%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,12 +15,9 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return root;
-        if(root.val == val){
-            return root;
+        while(root != null && root.val != val) {
+            root = (val < root.val) ? root.left : root.right;
         }
-        else{
-            return (val < root.val) ? searchBST(root.left,val) : searchBST(root.right,val);
-        }
+        return root;
     }
 }
