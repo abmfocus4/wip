4913040b2e0f3007489168fea836c459cfe7bb7e
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-04 20:29:29 -0400

    Time: 1 ms (90.78%) | Memory: 41.3 MB (64.68%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,11 +1,14 @@
+// converting s and t to char array and iterating is faster
 class Solution {
     public boolean isSubsequence(String s, String t) {
         if (s == null || s.isEmpty()) return true;
         // s is subsequence of t
         if (s.length() > t.length()) return false;
+        char[] s_str = s.toCharArray();
+        char[] t_str = t.toCharArray();
         int j = 0;
         for (int i = 0; i < t.length(); i++) {
-            if (t.charAt(i) == s.charAt(j)) j++;
+            if (t_str[i] == s_str[j]) j++;
             if (j == s.length()) return true;
         }
         return false;
