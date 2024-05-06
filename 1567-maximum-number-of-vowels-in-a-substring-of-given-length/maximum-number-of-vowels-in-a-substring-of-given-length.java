785e025aa64c7c113dc7100b5e5090cd927a910f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-05 21:11:17 -0400

    Time: 14 ms (62.03%) | Memory: 44.7 MB (32.50%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,20 +2,25 @@ class Solution {
         public int maxVowels(String s, int k) {
         int ans = 0;
         // Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-        var vowels = Set.of('a', 'e', 'i', 'o', 'u'); // Java 11 Collection factory method, credit to @Sithis
+        // var vowels = Set.of('a', 'e', 'i', 'o', 'u'); // Java 11 Collection factory method, credit to @Sithis
         int winCnt = 0;
         int start = 0;
         for (int i = 0; i < s.length(); ++i) {
-            if (vowels.contains(s.charAt(i))) {
+            if (isVowel(s.charAt(i))) {
                 ++winCnt; 
             }
             if (i-start == k) { // len of substr
-                if (vowels.contains(s.charAt(start))) // if contains vowels
+                if (isVowel(s.charAt(start))) // if contains vowels
                     --winCnt;
                 start++; // just pop from start of window
             }
             ans = Math.max(winCnt, ans);
+            if (ans == k) break; // max vowels you can have
         }
         return ans;
+    }
+
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
 }
