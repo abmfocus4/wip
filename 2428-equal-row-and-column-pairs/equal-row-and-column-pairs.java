03bf23aa9271b799ece1221afda3f0cf8cfe60b0
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-06 16:05:10 -0400

    Time: 3 ms (99.32%) | Memory: 47.2 MB (95.36%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,38 +1,22 @@
 class Solution {
     public int equalPairs(int[][] grid) {
-        HashMap<String, Integer> map = new HashMap(); // row string, frequency
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < m; i++) {
-            sb.setLength(0);
+        int n = grid.length, ans = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int hashVal = 0;
             for (int j = 0; j < n; j++) {
-                sb.append(grid[i][j]);
-                sb.append("#");
+                hashVal = grid[i][j] + 7 * hashVal;
             }
-            String row_str = sb.toString();
-            if (!map.containsKey(row_str)) {
-                map.put(row_str, 0);
-            }
-            map.put(row_str, map.get(row_str)+1);
+            mp.put(hashVal, mp.getOrDefault(hashVal, 0) + 1);
         }
-        
-        int pairs = 0;
         for (int j = 0; j < n; j++) {
-            sb.setLength(0);
-            for (int i = 0; i < m; i++) {
-                sb.append(grid[i][j]);
-                sb.append("#");
+            int hashVal = 0;
+            for (int i = 0; i < n; i++) {
+                hashVal = grid[i][j] + 7 * hashVal;
             }
-            String col_str = sb.toString();
-            if (map.containsKey(col_str)) {
-                pairs += map.get(col_str);
-            }
+            ans += mp.getOrDefault(hashVal,0);
         }
-        
-        return pairs;
+        return ans;
     }
 }
