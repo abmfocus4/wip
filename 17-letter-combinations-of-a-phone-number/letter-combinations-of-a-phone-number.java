fa9baf8461823d36c49db1898e79d21b5733bd67
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-08 11:06:23 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (38.05%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,21 +1,27 @@
-// https://leetcode.com/problems/letter-combinations-of-a-phone-number/solutions/3855474/100-backtracking-iterative-video-letter-combinations-of-a-phone-number
 class Solution {
-    public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) return new ArrayList();
-        List<String> combos = new ArrayList();
-        String[] map = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
-        backtrack(digits.toCharArray(), map, combos, "");
-        return combos;
+  public List<String> letterCombinations(String digits) {
+    if (digits.isEmpty())
+      return new ArrayList<>();
+
+    List<String> ans = new ArrayList<>();
+
+    dfs(digits, 0, new StringBuilder(), ans);
+    return ans;
+  }
+
+  private static final String[] digitToLetters = {"",    "",    "abc",  "def", "ghi",
+                                                  "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+  private void dfs(String digits, int i, StringBuilder sb, List<String> ans) {
+    if (i == digits.length()) {
+      ans.add(sb.toString());
+      return;
     }
 
-    public void backtrack(char[] digits, String[] map, List<String> combos, String temp_combo) {
-        if (temp_combo.length() == digits.length) {
-            combos.add(temp_combo);
-            return;
-        }
-        int digit = digits[temp_combo.length()] - '0';
-        for (char letter : map[digit].toCharArray()) {
-            backtrack(digits, map, combos, temp_combo + Character.toString(letter));
-        }
+    for (final char c : digitToLetters[digits.charAt(i) - '0'].toCharArray()) {
+      sb.append(c);
+      dfs(digits, i + 1, sb, ans);
+      sb.deleteCharAt(sb.length() - 1);
     }
+  }
 }
