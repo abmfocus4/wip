aee291ef14880a488fb09d3b89e83169c992794d
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-09 14:35:05 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (50.57%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,27 +1,25 @@
 class Solution {
-  public List<String> letterCombinations(String digits) {
-    if (digits.isEmpty())
-      return new ArrayList<>();
-
-    List<String> ans = new ArrayList<>();
-
-    dfs(digits, 0, new StringBuilder(), ans);
-    return ans;
-  }
-
-  private static final String[] digitToLetters = {"",    "",    "abc",  "def", "ghi",
+    private static final String[] map = {"",    "",    "abc",  "def", "ghi",
                                                   "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
-  private void dfs(String digits, int i, StringBuilder sb, List<String> ans) {
-    if (i == digits.length()) {
-      ans.add(sb.toString());
-      return;
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList();
+        if (digits == null || digits.isEmpty()) return res;
+        backtrack(digits, 0, res, new StringBuilder());
+        return res;
     }
-
-    for (final char c : digitToLetters[digits.charAt(i) - '0'].toCharArray()) {
-      sb.append(c);
-      dfs(digits, i + 1, sb, ans);
-      sb.deleteCharAt(sb.length() - 1);
+    
+    
+    private void backtrack(String digits, int i, List<String> res, StringBuilder sb) {
+        
+        if (sb.length() == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        
+        for (char c : map[digits.charAt(i) - '0'].toCharArray()) {
+            sb.append(c);
+            backtrack(digits, i + 1, res, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
-  }
 }
