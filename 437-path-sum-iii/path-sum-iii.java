fcec7f4092a6ad31cbfddaa39fd31a61d1edd9e6
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-10 14:50:38 -0400

    Time: 3 ms (96.80%) | Memory: 44.4 MB (19.65%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -1,20 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int pathSum(TreeNode root, int sum) {
-        if (root == null) return 0;
-        return pathSumFromNode(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+     Map<Long, Integer> hmap;
+    int count;
+
+    public int pathSum(TreeNode root, int targetSum) {
+        hmap = new HashMap<>();
+        count = 0;
+
+        dfs(root, 0, targetSum);
+
+        return count;
     }
-    
-    public int pathSumFromNode(TreeNode node, long sum) { // make sum long
-        int result = 0;
+
+    private void dfs(TreeNode root, long prefixSum, int targetSum) {
+        // base case
+        if (root == null) return;
+
         
-        if (node == null) return 0;
-        if (node.val == sum) {
-            result += 1;
+        prefixSum += root.val;
+        
+        // If map contains a sum equal to (prefixSum - targetSum), we need to increment count that many times
+        if (hmap.containsKey(prefixSum-targetSum)) {
+            count += hmap.get(prefixSum-targetSum);
+        } 
+        
+        // There can be cases when the prefixSum is directly equal to targetSum, we need to increment count
+        if (targetSum == prefixSum) {
+            count++;
         }
+
+        // Update the prefixSum till current node and it's count
+        hmap.put(prefixSum, hmap.getOrDefault(prefixSum, 0) + 1);
+
+        // Recurse
+        dfs(root.left, prefixSum, targetSum);
+        dfs(root.right, prefixSum, targetSum);
+
+        // Backtrack
+        hmap.put(prefixSum, hmap.get(prefixSum) - 1);
         
-        result += pathSumFromNode(node.left, sum - node.val);
-        result += pathSumFromNode(node.right, sum - node.val);
-        
-        return result;
     }
 }
