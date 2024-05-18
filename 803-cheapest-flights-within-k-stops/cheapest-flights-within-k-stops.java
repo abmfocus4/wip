74a47b9aa9db6af6bf663438bc5a11c211c60be2
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-18 12:55:27 -0400

    Time: 5 ms (97.25%) | Memory: 43.9 MB (87.48%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,59 +1,25 @@
-// https://www.youtube.com/watch?v=HBfvhLlO_po&ab_channel=AryanMittal
-// Modified Dijkstra's
 class Solution {
-    class Edge {
-        int dst;
-        int price;
-
-        public Edge(int dst, int price) {
-            this.dst = dst;
-            this.price = price;
-        }
-    }
-
-    class PQElement {
-        int price;
-        int dst;
-        int steps;
-
-        public PQElement (int price, int dst, int steps) {
-            this.price = price;
-            this.dst = dst;
-            this.steps = steps;
-        }
-    }
+    // bellman ford bfs
+    // https://www.youtube.com/watch?v=5eIK3zUdYmE&ab_channel=NeetCode
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        // create adj list
-        ArrayList<ArrayList<Edge>> adj_list = new ArrayList();
+        int[] prices = new int[n];
+        Arrays.fill(prices, Integer.MAX_VALUE);
+        prices[src] = 0;
 
-        for (int i = 0; i < n; i++) {
-            adj_list.add(new ArrayList());
-        }
+        for (int i = 0; i < k+1; i++) {
+            int[] tmpPrices = Arrays.copyOf(prices, n);
 
-        for (int[] flight : flights) {
-            int flight_src = flight[0], flight_dst = flight[1], flight_price = flight[2];
-            adj_list.get(flight_src).add(new Edge(flight_dst, flight_price));
-        }
-
-        int[] stops = new int[n]; // steps taken to reach stop from src
-        Arrays.fill(stops, Integer.MAX_VALUE);
-
-        // {price, dst node, steps}
-        PriorityQueue<PQElement> pq = new PriorityQueue<>((a,b) -> a.price - b.price);
-        pq.add(new PQElement(0, src, 0));
-
-        while (pq.isEmpty() == false) {
-            PQElement cur = pq.poll();
-            // steps = stop + 1
-            if (cur.steps > stops[cur.dst] || cur.steps > k+1) continue;
-            stops[cur.dst] = cur.steps;
-            if (cur.dst == dst) return cur.price;
-            for (Edge neighbor : adj_list.get(cur.dst)) {
-                pq.add(new PQElement(cur.price + neighbor.price, neighbor.dst, cur.steps + 1));
+            for (int[] flight : flights) { //src, dst, price
+                int flight_src = flight[0], flight_dst = flight[1], flight_price = flight[2];
+                if (prices[flight_src] == Integer.MAX_VALUE) continue;
+                if (prices[flight_src] + flight_price < tmpPrices[flight_dst]) {
+                    tmpPrices[flight_dst] = prices[flight_src] + flight_price;
+                }
             }
+
+            prices = Arrays.copyOf(tmpPrices, n); // copy tmp prices to prices
         }
 
-        return -1;
-
+        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
     }
 }
