d4b6831a37dd54010e84f93e47c9f96c5f3b1772
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-20 19:06:43 -0400

    Time: 2 ms (100.00%) | Memory: 40.5 MB (98.09%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,18 +1,13 @@
-// knapsack probs: https://www.youtube.com/watch?v=mBNrRy2_hVs&ab_channel=NeetCode
-// ref: https://www.youtube.com/watch?v=Mjy4hd2xgrs&ab_channel=NeetCode
-
-// best ref: https://leetcode.com/problems/coin-change-ii/solutions/99212/knapsack-problem-java-solution-with-thinking-process-o-nm-time-and-o-m-space
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[coins.length + 1][amount + 1];
-        dp[0][0] = 1;
-        for (int i = 1; i <= coins.length; i++) {
-            dp[i][0] = 1;
-            for (int j = 1; j <= amount; j++) {
-                dp[i][j] += ((j - coins[i-1] >= 0) ? dp[i][j - coins[i-1]] : 0) + dp[i-1][j];
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
             }
         }
-        return dp[coins.length][amount];
-
+        return dp[amount];
     }
 }
