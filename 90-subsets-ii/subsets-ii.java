88e4506d529b4a4a446def00ab2fa1fe41e649d9
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-20 22:49:26 -0400

    Time: 1 ms (99.81%) | Memory: 43.2 MB (70.46%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, 0);
+    return list;
+}
+
+private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+    list.add(new ArrayList<>(tempList));
+    for(int i = start; i < nums.length; i++){
+        if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, i + 1);
+        tempList.remove(tempList.size() - 1);
+    }
+} 
+}
