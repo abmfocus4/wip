950198722cd8c698477fec776f490dc07e620232
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-20 20:41:38 -0400

    Time: 0 ms (100.00%) | Memory: 42.8 MB (80.83%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<String> res;
+    public List<String> generateParenthesis(int n) {
+        this.res = new ArrayList();
+        char[] parenthesis = new char[n*2];
+        generateParenthesis(n, n, 0, parenthesis);
+        return res;
+        // if there are left rem
+        // if the left at most right
+    }
+
+    private void generateParenthesis(int leftRem, int rightRem, int index, char[] parenthesis) {
+        if (leftRem < 0 || rightRem < leftRem) {
+            return;
+        }
+
+        if (leftRem == 0 && rightRem == 0) {
+            res.add(String.copyValueOf(parenthesis));
+        } else {
+            parenthesis[index] = '(';
+            generateParenthesis(leftRem - 1, rightRem, index+1, parenthesis);
+
+            parenthesis[index] = ')';
+            generateParenthesis(leftRem, rightRem - 1, index+1, parenthesis);
+        }
+        
+    }
+}
