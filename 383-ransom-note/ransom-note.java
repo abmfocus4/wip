3a86d7c09e82854523e2b85696a0d76143db65fc
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-22 22:50:16 -0400

    Time: 2 ms (81.02%) | Memory: 44.4 MB (81.93%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,17 @@
+
+// https://leetcode.com/problems/ransom-note/solutions/1671552/1ms-100-easy-explanation-java-solution
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
+        }
+        return true;
+    }
+}
