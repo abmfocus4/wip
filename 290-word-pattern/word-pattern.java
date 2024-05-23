d5ffb3f2042ad82b3a9bd5dda131f399f7b781cd
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-22 23:11:44 -0400

    Time: 1 ms (75.43%) | Memory: 41 MB (92.08%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/word-pattern/solutions/73402/8-lines-simple-java
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+    String[] words = str.split(" ");
+    if (words.length != pattern.length())
+        return false;
+    Map index = new HashMap();
+    for (Integer i=0; i<words.length; ++i)
+        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+            return false;
+    return true;
+}
+}
