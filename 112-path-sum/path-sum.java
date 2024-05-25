682c22e4fe98923eefa10bf59d50735c58844772
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-25 19:24:50 -0400

    Time: 0 ms (100.00%) | Memory: 43 MB (54.17%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//  https://www.youtube.com/watch?v=LSKQyOz_P8I&ab_channel=NeetCode
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null) return (targetSum - root.val) == 0;
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
