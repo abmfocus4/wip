5a1c98a18c595ff89fbf57ab7e17e5c08fe608be
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-26 00:47:06 -0400

    Time: 0 ms (100.00%) | Memory: 41.4 MB (85.98%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,22 @@
+class Solution {
+public int[] plusOne(int[] digits) {
+        
+    int n = digits.length;
+    for(int i=n-1; i>=0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        
+        digits[i] = 0;
+    }
+
+// if all number inside array are 9
+// i.e. [ 9,9,9,9 ] than according to above loop it will become [ 0,0,0,0 ]
+// but we have to make it like this [ 9,9,9,9 ]-->[ 1,0,0,0,0 ]
+    int[] newNumber = new int [n+1];
+    newNumber[0] = 1;
+    
+    return newNumber;
+}
+}
