138cad7962a61f545638642b264d1cc4c7198420
Meg Alapati <bmalapat@uwaterloo.ca>
2024-05-27 23:35:17 -0400

    Time: 453 ms (70.46%) | Memory: 46.4 MB (7.00%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,63 +1,67 @@
 class Solution {
+    String s;
+
     public List<Integer> findSubstring(String s, String[] words) {
+        this.s = s;
         Trie trie = new Trie(words);
         List<Integer> res = new ArrayList<>();
-        for(int i = 0; i <= s.length() - words.length; i++) {
-            if(trie.dfs(s, i, trie.root) >= words.length) {
+        for (int i = 0; i <= s.length() - words.length; i++) {
+            if (trie.dfs(i, trie.root) >= words.length) {
                 res.add(i);
             }
         }
         return res;
     }
-}
 
-class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean endOfWord;
-    boolean visited;
-    int wordCount;
-    TrieNode() {
-        children = new HashMap<>();
-        endOfWord = false;
-        visited = false;
-        wordCount = 0;
-    }
-}
+    class TrieNode {
+        Map<Character, TrieNode> children;
+        boolean endOfWord;
+        int wordCount;
 
-class Trie {
-    TrieNode root;
-    Trie(String[] words) {
-        root = new TrieNode();
-        for(String word: words)
-            insert(word);
+        TrieNode() {
+            children = new HashMap<>();
+            endOfWord = false;
+            wordCount = 0;
+        }
     }
-    
-    public void insert(String s) {
-        TrieNode cur = root;
-        for(char ch: s.toCharArray()) {
-            TrieNode node = cur.children.get(ch);
-            if(node == null) {
-                node = new TrieNode();
-                cur.children.put(ch, node);
+
+    class Trie {
+        TrieNode root;
+
+        Trie(String[] words) {
+            root = new TrieNode();
+            for (String word : words)
+                insert(word);
+        }
+
+        public void insert(String s) {
+            TrieNode cur = root;
+            for (char ch : s.toCharArray()) {
+                TrieNode node = cur.children.get(ch);
+                if (node == null) {
+                    node = new TrieNode();
+                    cur.children.put(ch, node);
+                }
+                cur = node;
             }
-            cur = node;
-        }
-        cur.wordCount++;
-        cur.endOfWord = true;
-    }
-    
-    public int dfs(String s, int idx, TrieNode node) {
-        if(idx >= s.length() || node == null)   
-            return 0;
-        char ch = s.charAt(idx);
-        TrieNode cur = node.children.get(ch);
-        if(cur == null)     return 0;
-        if(cur.endOfWord && cur.wordCount > 0) {
-            cur.wordCount--;
-            int res = 1 + dfs(s, idx + 1, root);
             cur.wordCount++;
-            return res;
+            cur.endOfWord = true;
         }
-        return dfs(s, idx + 1, cur);
+
+        public int dfs(int idx, TrieNode node) {
+            if (idx >= s.length() || node == null)
+                return 0;
+            char ch = s.charAt(idx);
+            TrieNode cur = node.children.get(ch);
+            if (cur == null)
+                return 0;
+            if (cur.endOfWord && cur.wordCount > 0) {
+                cur.wordCount--;
+                int res = 1 + dfs(idx + 1, root);
+                cur.wordCount++; // backtrack
+                return res;
+            }
+            return dfs(idx + 1, cur); // continue with current node
+        }
     }
 }
