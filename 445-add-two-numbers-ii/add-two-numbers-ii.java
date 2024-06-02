8e06a3e3d258276cbe125f5154e2d11fd17f4694
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-02 00:56:48 -0400

    Time: 3 ms (37.58%) | Memory: 44.7 MB (19.30%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -10,31 +10,29 @@
  * }
  */
 class Solution {
-    private ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
-            ListNode nextNode = head.next;
-            head.next = prev;
-            prev = head;
-            head = nextNode;
-        }
-        return prev;
-    }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        l1 = reverse(l1);
-        l2 = reverse(l2);
+        Stack<Integer> s1 = new Stack();
+        Stack<Integer> s2 = new Stack();
+        
+        while (l1 != null) { // push all l1 elements into stack
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
 
         int sum = 0, carry = 0;
         ListNode ans = new ListNode(0);
-        while (l1 != null || l2 != null) {
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
+        while (s1.isEmpty() == false || s2.isEmpty() == false) {
+            if (s1.isEmpty() == false) {
+                sum += s1.pop();
             }
 
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
+            if (s2.isEmpty() == false) {
+                sum += s2.pop();
             }
             ans.val = sum % 10;
             carry = sum / 10;
