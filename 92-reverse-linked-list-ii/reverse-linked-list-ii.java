4fd8a8c6be74cabecb931e4ec56907a46a7b4367
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-02 17:15:26 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.39%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,31 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//  https://www.youtube.com/watch?v=bRZ_Fy4zRRY&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=10&ab_channel=codestorywithMIK
 class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        if (head == null)
-            return null;
-
-        ListNode dummy = new ListNode(0); // create a dummy node to mark the head of this list
-        dummy.next = head;
-        ListNode pre = dummy; // make a pointer pre as a marker for the node before reversing
-        
-        for (int i = 0; i < m - 1; i++)
-            pre = pre.next;
-
-        ListNode start = pre.next; // a pointer to the beginning of a sub-list that will be reversed
-        ListNode then = start.next; // a pointer to a node that will be reversed
-
-        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
-        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
-
-        for (int i = 0; i < n - m; i++) {
-            start.next = then.next;
-            then.next = pre.next;
-            pre.next = then;
-            then = start.next;
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || head.next == null) {
+            return head;
         }
 
-        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
-        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5
-        // (finish)
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy; // if left is head then prev is dummy
+        for (int i = 1; i < left; i++) { // set prev node
+            prev = prev.next;
+        }
+
+        ListNode cur = prev.next;
+        for (int i = 1; i <= (right - left); i++) {
+            ListNode temp = prev.next;
+            prev.next = cur.next;
+            cur.next = cur.next.next;
+            prev.next.next = temp;
+        }
 
         return dummy.next;
     }
