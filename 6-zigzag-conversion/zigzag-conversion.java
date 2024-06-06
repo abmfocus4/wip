16c2fa41f9fcedaaa3524f02c84cd63319a0e319
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-05 23:45:59 -0400

    Time: 4 ms (81.67%) | Memory: 44.6 MB (77.55%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,14 +1,15 @@
+// TC: O(n)
 class Solution {
     public String convert(String s, int numRows) {
         if (numRows <= 1) return s;
 
-        StringBuilder[] sbs = new StringBuilder[numRows];
+        StringBuilder[] sbs = new StringBuilder[numRows]; // each row is string builder
         for (int i = 0; i < sbs.length; i++) sbs[i] = new StringBuilder();
 
         int idx = 0;
         int direction = -1;
         char[] chars = s.toCharArray();
-        for (char c : chars) {
+        for (char c : chars) { // append row by row
             sbs[idx].append(c);
             if (idx == 0 || idx == numRows - 1) direction = 0 - direction; // hit bottom or top row, change direction
             idx += direction;
