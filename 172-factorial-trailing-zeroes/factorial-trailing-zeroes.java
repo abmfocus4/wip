222f8ffc901b1bc95af68458bd2e1b9f1313c80c
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-06 22:44:23 -0400

    Time: 0 ms (100.00%) | Memory: 40.1 MB (96.04%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,12 +1,5 @@
-// https://www.youtube.com/watch?v=57hXUDXVY8s&list=TLPQMDcwNjIwMjSq9lKrG3yYNQ&index=3&ab_channel=TAPACADEMY
-public class Solution {
+class Solution {
     public int trailingZeroes(int n) {
-        int count = 0;
-        while (n != 0) {
-            int tmp = n / 5;
-            count += tmp;
-            n = tmp;
-        }
-        return count;
+            return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
     }
 }
