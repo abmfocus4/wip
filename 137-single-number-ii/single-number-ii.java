3ab5e4c5eb2480fbebb121d71a141aec1f039329
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-06 20:30:25 -0400

    Time: 1 ms (83.35%) | Memory: 45.1 MB (81.46%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,26 +1,11 @@
-// https://www.youtube.com/watch?v=LK0c7tCzKmM&list=TLPQMDYwNjIwMjTGU_sP9Ubjew&index=4&ab_channel=codestorywithMIK
+// https://leetcode.com/problems/single-number-ii/solutions/3714928/bit-manipulation-c-java-python-beginner-friendly
 class Solution {
     public int singleNumber(int[] nums) {
-        // TC: O(32*n)
-        int result = 0;
-        for (int k = 0; k <= 31; k++) {
-            int mask = 1 << k;
-
-            int countZeroes = 0;
-            int countOnes = 0;
-
-            for (int num : nums) {
-                if ((num & mask) == 0) {
-                    countZeroes++;
-                } else {
-                    countOnes++;
-                }
-            }
-            if ((countOnes % 3) == 1) {
-                result |= mask;
-            }
+        int seenOnce = 0, seenTwice = 0;
+        for (int num : nums) {
+            seenOnce = (seenOnce ^ num) & (~seenTwice);
+            seenTwice = (seenTwice ^ num) & (~seenOnce);
         }
-
-        return result;
+        return seenOnce;
     }
 }
