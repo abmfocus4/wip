cb29f7ec54c191f2adcdabab71d302404118f00c
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-09 10:59:17 -0400

    Time: 0 ms (100.00%) | Memory: 44 MB (99.27%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,19 +1,13 @@
 class Solution {
+    // for non repeating reflective pairs (no i,i and i,j == j,i)
+    // brute force
     public int[] twoSum(int[] nums, int target) {
-        // hash table stores index and element
-        // iterate through array
-        // check if complement exists in ht
-        // if it does then return cur index and output index from ht
-        // otherwise put cur index in ht
-        // return null if no complement
-
-        HashMap<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
+       for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target) {
+                    return new int[]{j, j - i};
+                }
             }
-            map.put(nums[i], i);
         }
         return null;
     }
