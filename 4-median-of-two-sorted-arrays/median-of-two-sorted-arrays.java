b6bf881c292dbf317bfe98328a05745e555d3c7c
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-09 12:28:30 -0400

    Time: 1 ms (100.00%) | Memory: 45.5 MB (95.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,33 +1,66 @@
-// Brute force approach
-// TC: O(m + n) == SC
+// Better approach
+// TC: O(m + n), SC: O(1)
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int total = m + n;
-        int[] nums = new int[total];
+        int size = m + n;
+
         int i = 0, j = 0, k = 0;
+        int idx1 = size/2;
+        int elem1 = 0;
+
+        int idx2 = size/2 - 1;
+        int elem2 = 0;
 
         while (i < m && j < n) {
             if (nums1[i] < nums2[j]) {
-                nums[k++] = nums1[i++];
+                if (k == idx1) {
+                    elem1 = nums1[i];
+                }
+                if (k == idx2) {
+                    elem2 = nums1[i];
+                }
+                i++;
+                k++;
             } else {
-                nums[k++] = nums2[j++];
+                if (k == idx1) {
+                    elem1 = nums2[j];
+                }
+                if (k == idx2) {
+                    elem2 = nums2[j];
+                }
+                j++;
+                k++;
             }
         }
 
         while (i < m) {
-            nums[k++] = nums1[i++];
+            if (k == idx1) {
+                elem1 = nums1[i];
+            }
+            if (k == idx2) {
+                elem2 = nums1[i];
+            }
+            i++;
+            k++;
         }
 
         while (j < n) {
-            nums[k++] = nums2[j++];
+            if (k == idx1) {
+                elem1 = nums2[j];
+            }
+            if (k == idx2) {
+                elem2 = nums2[j];
+            }
+            j++;
+            k++;
         }
 
-        if (total % 2 == 0) { // even
-            return (nums[total/2] + nums[total/2 - 1])/2.0;
+        if (size % 2 == 0) { // even
+            return (elem2 + elem1)/2.0;
         } else { // odd
-            return nums[total/2];
+            return elem1;
         }
     }
 }
