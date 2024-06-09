0d0199d698a27b7bb245df0ffb3eb48b40d0bf3f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-09 12:52:29 -0400

    Time: 1 ms (100.00%) | Memory: 45.9 MB (42.22%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,66 +1,45 @@
-// Better approach
-// TC: O(m + n), SC: O(1)
+// simple approach - https://www.youtube.com/watch?v=6D9T2ZY8h5c&list=TLPQMjYwNTIwMjRZnxFRIBCHUQ&index=12&ab_channel=codestorywithMIK
+// https://www.youtube.com/watch?v=7nABqJCEMuY&list=TLPQMjYwNTIwMjRZnxFRIBCHUQ&index=13&ab_channel=codestorywithMIK
+
+// Approach 3: Binary Search
+// Use binary search to partition the smaller of the two input arrays into two parts.
+// Find the partition of the larger array such that the sum of elements on the left side of the partition in both arrays is half of the total elements.
+// Check if this partition is valid by verifying if the largest number on the left side is smaller than the smallest number on the right side.
+// If the partition is valid, calculate and return the median.
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int size = m + n;
+        // consider nums1 to be smaller than nums2
+        if (nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
 
-        int i = 0, j = 0, k = 0;
-        int idx1 = size/2;
-        int elem1 = 0;
+        int n1 = nums1.length, n2 = nums2.length;
+        int l = 0, r = n1; // you can take 0 elems from nums1 or max n1 elems from nums1
+        int n = n1 + n2;
 
-        int idx2 = size/2 - 1;
-        int elem2 = 0;
+        while (l <= r) {
+            int Px = (l + r) >> 1;
+            int Py = (n1 + n2 + 1) / 2 - Px; // left
 
-        while (i < m && j < n) {
-            if (nums1[i] < nums2[j]) {
-                if (k == idx1) {
-                    elem1 = nums1[i];
-                }
-                if (k == idx2) {
-                    elem2 = nums1[i];
-                }
-                i++;
-                k++;
+            int l1 = Px == 0 ? Integer.MIN_VALUE : nums1[Px - 1]; // took no elems on the left from nums1
+            int l2 = Py == 0 ? Integer.MIN_VALUE : nums2[Py - 1];
+            int r1 = Px == n1 ? Integer.MAX_VALUE : nums1[Px]; // took all elems on the left, none on right
+            int r2 = Py == n2 ? Integer.MAX_VALUE : nums2[Py];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) // total odd number elems in list
+                    return Math.max(l1, l2);
+                else
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            }
+
+            if (l1 > r2) { // took too many elements on left, move one to right, move partition Px to left by 1
+                r = Px - 1;
             } else {
-                if (k == idx1) {
-                    elem1 = nums2[j];
-                }
-                if (k == idx2) {
-                    elem2 = nums2[j];
-                }
-                j++;
-                k++;
+                l = Px + 1;
             }
+
         }
 
-        while (i < m) {
-            if (k == idx1) {
-                elem1 = nums1[i];
-            }
-            if (k == idx2) {
-                elem2 = nums1[i];
-            }
-            i++;
-            k++;
-        }
-
-        while (j < n) {
-            if (k == idx1) {
-                elem1 = nums2[j];
-            }
-            if (k == idx2) {
-                elem2 = nums2[j];
-            }
-            j++;
-            k++;
-        }
-
-        if (size % 2 == 0) { // even
-            return (elem2 + elem1)/2.0;
-        } else { // odd
-            return elem1;
-        }
+        return 0;
     }
 }
