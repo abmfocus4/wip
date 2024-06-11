509fc7d2ea18c1bf4aa8c6cfb704d6e4d592c2e8
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-10 23:04:23 -0400

    Time: 0 ms (100.00%) | Memory: 42.2 MB (15.11%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,17 +1,14 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
-
-        // find smallest element
+        // find smallest element - find pivot index
         int left = 0;
         int right = nums.length - 1;
-        
         while (left < right) {
-            int midpoint = left + (right - left)/2;
-            if (nums[midpoint] > nums[right]) {
-                left = midpoint + 1;
+            int mid = left + (right - left)/2;
+            if (nums[mid] >  nums[right]) {
+                left = mid + 1;
             } else {
-                right = midpoint;
+                right = mid;
             }
         }
 
@@ -26,13 +23,13 @@ class Solution {
         }
 
         while (left <= right) {
-            int midpoint = left + (right - left)/2;
-            if (nums[midpoint] == target) {
-                return midpoint;
-            } else if (nums[midpoint] > target) {
-                right = midpoint - 1;
+            int mid = (left + right)/2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
             } else {
-                left = midpoint + 1;
+                left = mid + 1;
             }
         }
 
