dcccf07e98934282007fae540b7f83bccc27df01
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-11 23:01:48 -0400

    Time: 2 ms (78.17%) | Memory: 44.5 MB (90.31%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -11,7 +11,7 @@ class Solution {
             list.add(new ArrayList<>(tempList));
         } else {
             for (int i = 0; i < nums.length; i++) {
-                if (used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]))
+                if (used[i] || (i > 0 && nums[i] == nums[i - 1] && used[i - 1]))
                     continue;
                 used[i] = true;
                 tempList.add(nums[i]);
