dec62e49c7732ce43fde844730c2a6243e4ed679
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-16 00:30:19 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (55.94%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,35 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-// https://www.youtube.com/watch?v=sWf7k1x9XR4&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=19&ab_channel=takeUforward
 class Solution {
+    // brute force O(N) space and time
+    // reverse post order
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        TreeNode cur = root;
-        TreeNode prev = null;
-        while (cur != null) {
-            if (cur.left != null) {
-                prev = cur.left;
-                while (prev.right != null) { // find right most node
-                    prev = prev.right;
-                }
-                prev.right = cur.right;
-
-                cur.right = cur.left;
-                cur.left = null;
-            }
-            cur = cur.right;
+        if (root == null) {
+            return;
         }
+
+        flatten(root.right);
+        flatten(root.left);
+
+
+        root.right = prev;
+        root.left = null;
+        prev = root;
+
     }
 }
