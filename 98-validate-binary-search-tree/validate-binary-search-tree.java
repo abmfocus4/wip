eea26cff3f63f45be9c065cffeebf241f2e7e103
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-15 21:10:56 -0400

    Time: 1 ms (24.42%) | Memory: 43 MB (84.08%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -13,23 +13,41 @@
  *     }
  * }
  */
+//  https://www.youtube.com/watch?v=80Zug6D1_r4&list=TLPQMTYwNjIwMjTwkIyyJmy5Kg&index=1&ab_channel=takeUforward
+// Morris Traversal
+// Threaded Binary Tree Concept
+// O(n) amortized time, O(1) space
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        // inorder traversal
-        // make sure nodes on the right subtrees are always greater than root nodes
-        if (root == null) return false;
-        TreeNode pre = null;
-        Stack<TreeNode> stack = new Stack();
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
+        TreeNode cur = root;
+        TreeNode comp = null;
+        while (cur != null) {
+            if (cur.left == null) { // case 1:
+                if(comp != null && comp.val >= cur.val){
+                    return false;
+                }
+                comp = cur;                
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left; // left subtree
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right; // go to the rightmost child of left subtree
+                }
+                if (prev.right == null) { // case 2: first visit
+                    prev.right = cur; // make the threaded connection
+                    cur = cur.left; // traverse left
+                } else { // case 3: threaded connection already exists
+                    prev.right = null; // remove connection
+                    if(comp != null && comp.val >= cur.val){
+                        return false;
+                    }
+                    comp = cur;                
+                    cur = cur.right; // move to right (LN)R
+                }
+
             }
-            root = stack.pop();
-            if (pre != null && pre.val >= root.val) return false;
-            pre = root;
-            root = root.right;
         }
+
         return true;
     }
 }
