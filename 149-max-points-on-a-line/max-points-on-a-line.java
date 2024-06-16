5fecdfe5923f2627f0debbf400c3a95e61786cbf
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-16 18:13:44 -0400

    Time: 35 ms (26.40%) | Memory: 41 MB (93.46%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -1,43 +1,35 @@
-// time comp - O(n2) & space comp - O(n)
-// https://www.youtube.com/watch?v=AzER0wuL0QY&list=TLPQMjYwNTIwMjRZnxFRIBCHUQ&index=11&ab_channel=codestorywithMIK
 class Solution {
     public int maxPoints(int[][] points) {
-        if (points == null) {
-            return 0;
-        }
-        int totalPoints = 0;
+        // 2 points make a line
+        // get dy and dx from that point
+        // use the rest of the array to match the rwo points slope
+
+
         int n = points.length;
-        if (n <= 1) {
-            return n;
-        }
-        for (int i = 0; i < n; i++) {
-            int max_count = 0;
-            Map<Pair<Integer, Integer>, Integer> map = new HashMap(); // dx,dy slope and freq
-            // use map.clear() and declare map outside instead
-            for (int j = i+1; j < n; j++) {
-                // x2 - x1
-                int deltaX = points[j][0] - points[i][0];
-                int deltaY = points[j][1] - points[i][1];
+        if (n <= 1) return n;
 
-                int gcd = gcd(deltaX, deltaY); 
-                int dx = deltaX/gcd;
-                int dy = deltaY/gcd;
-                Pair slope = new Pair(dy, dx);
+        int maxPoints = 0;
 
-                map.put(slope, map.getOrDefault(slope, 0) + 1);
-                max_count = Math.max(max_count, map.get(slope));
+        for (int i = 0; i < n; i++) { // x1
+            for (int j = i+1; j < n; j++) { // x2
+                int count = 2;
+
+                int dx = points[j][0] - points[i][0]; // x2 - x1
+                int dy = points[j][1] - points[i][1]; // y2 - y1
+
+                for (int k = 0; k < n; k++) {
+                    if (k == i || k == j) continue;
+
+                    int dx_ = points[k][0] - points[i][0]; // x3 - x1
+                    int dy_ = points[k][1] - points[i][1]; // y3 - y1
+                    if (dx * dy_ == dy * dx_) count++;
+                }
+
+                maxPoints = Math.max(maxPoints, count);
             }
-            totalPoints = Math.max(max_count + 1, totalPoints); // add the ith point too
-            
         }
 
-        return totalPoints;
-    }
-
-    private int gcd (int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a%b);
+        return maxPoints;
+         
     }
 }
