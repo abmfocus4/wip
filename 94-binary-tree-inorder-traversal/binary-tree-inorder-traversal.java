28664d2824740aab02b097789dcce56b637b70de
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-15 20:44:55 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (85.40%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,34 +1,47 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-// Left -> Root -> Right
+//  https://www.youtube.com/watch?v=80Zug6D1_r4&list=TLPQMTYwNjIwMjTwkIyyJmy5Kg&index=1&ab_channel=takeUforward
+// Morris Traversal
+// Threaded Binary Tree Concept
+// O(n) amortized time, O(1) space
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList();
-        if (root == null)
-            return list;
-        Stack<TreeNode> stack = new Stack();
-        while (root != null || !stack.isEmpty()) {
-            while (root != null) {
-                stack.push(root);
-                root = root.left;
+        List<Integer> res = new ArrayList();
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) { // case 1:
+                res.add(cur.val);
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left; // left subtree
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right; // go to the rightmost child of left subtree
+                }
+                if (prev.right == null) { // case 2: first visit
+                    prev.right = cur; // make the threaded connection
+                    cur = cur.left; // traverse left
+                } else { // case 3: threaded connection already exists
+                    prev.right = null; // remove connection
+                    res.add(cur.val);
+                    cur = cur.right; // move to right (LN)R
+                }
+
             }
-            root = stack.pop();
-            list.add(root.val);
-            root = root.right;
         }
-        return list;
+
+        return res;
     }
 }
