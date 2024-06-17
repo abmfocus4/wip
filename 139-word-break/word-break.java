41eb604f179dc84a81a06b959b6143486afad1c2
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-16 21:51:06 -0400

    Time: 3 ms (83.25%) | Memory: 42.6 MB (68.19%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,39 +1,47 @@
 class Solution {
-    private String s;
-    private List<String> wordDict;
-    private int[] memo;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        // Trie Tree
+        class Trie {
+            HashMap<Character, Trie> children;
+            boolean isWord;
 
-    private boolean dp(int i) {
-        if (i < 0) return true;
-
-        if (memo[i] != -1) {
-            return memo[i] == 1;
-        }
-
-        for (String word : wordDict) {
-            // Handle out of bounds case
-            // if (i - word.length() + 1 < 0) {
-            //     continue;
-            // }
-
-            if ( (i - word.length() + 1 >= 0) && 
-                s.substring(i - word.length() + 1, i + 1).equals(word) &&
-                dp(i - word.length())
-            ) {
-                memo[i] = 1;
-                return true;
+            public Trie() {
+                this.children = new HashMap();
+                this.isWord = false;
             }
         }
 
-        memo[i] = 0;
-        return false;
-    }
+        Trie root = new Trie();
 
-    public boolean wordBreak(String s, List<String> wordDict) {
-        this.s = s;
-        this.wordDict = wordDict;
-        this.memo = new int[s.length()];
-        Arrays.fill(this.memo, -1);
-        return dp(s.length() - 1);
+        // insert all words in wordDict in Tree
+        for (String word : wordDict) {
+            Trie cur = root;
+            for (Character c : word.toCharArray()) {
+                if (!cur.children.containsKey(c)) {
+                    cur.children.put(c, new Trie());
+                }
+                cur = cur.children.get(c);
+            }
+            cur.isWord = true;
+        }
+
+        boolean[] dp = new boolean[s.length()];
+        Arrays.fill(dp, false);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || dp[i-1]) {
+                Trie cur = root;
+                for (int j = i; j < s.length(); j++) {
+                    char c = s.charAt(j);
+                    if (!cur.children.containsKey(c)) break;
+                    cur = cur.children.get(c);
+                    if (cur.isWord) {
+                        dp[j] = true;
+                    }
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
     }
 }
