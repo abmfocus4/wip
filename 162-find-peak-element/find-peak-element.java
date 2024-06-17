c18b6b3992859bf147f4751bc449ce87b062123b
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-16 22:00:50 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (73.94%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -4,8 +4,12 @@ class Solution {
         // step in the direction of the ascend to reach the peak
         int N = nums.length;
 
-        if (N == 1) return 0;
+        // if only one element in the list
+        if (N == 1) {
+            return 0;
+        }
 
+        // check both the boundaries
         if (nums[0] > nums[1]) return 0;
         if (nums[N - 1] > nums[N - 2]) return N-1;
         
@@ -14,12 +18,15 @@ class Solution {
 
         while (left <= right) {
             int mid = left + (right - left)/2;
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            } else if (nums[mid - 1] > nums[mid]) { // search left space
+            // search on the side of the array which is larger than the mid
+            // then we can guarantee that there will be a peak element
+            
+            if (nums[mid - 1] > nums[mid]) { // search left space
                 right = mid - 1;
             } else if (nums[mid + 1] > nums[mid]) { // search right space
                 left = mid + 1;
+            } else {
+                return mid;
             }
         }
         return -1;
