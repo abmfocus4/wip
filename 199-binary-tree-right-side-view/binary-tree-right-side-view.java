5f3fa27e55a83824a716ed9666bf67fe74bbf6c1
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-18 00:00:08 -0400

    Time: 0 ms (100.00%) | Memory: 42.6 MB (6.63%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,25 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        // level order traversal, let the last element in the level
         List<Integer> result = new ArrayList();
-        Queue<TreeNode> q = new LinkedList();
-        if (root != null) q.add(root);
-        while (q.isEmpty() == false) {
-            int levelLength = q.size();
-            int val = 0;
-            for (int i = 0; i < levelLength; i++) {
-                TreeNode cur = q.poll();
-                val = cur.val;
-                if (cur.left != null) {
-                    q.add(cur.left);
-                }
-                if (cur.right != null) {
-                    q.add(cur.right);
-                }
-            }
-            result.add(val);
+        rightSideView(root, result, 0);
+        return result;
+    }
+
+    public void rightSideView(TreeNode cur, List<Integer> result, int depth) {
+        if (cur == null) {
+            return;
         }
 
-        return result;
+        if (result.size() == depth) { // if you are at depth then add value
+            result.add(cur.val);
+        }
+
+        rightSideView(cur.right, result, depth+1); // check right
+        rightSideView(cur.left, result, depth+1); // check left
     }
 }
