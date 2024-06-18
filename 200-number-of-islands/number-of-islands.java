667a882dfbe2b9cb2fbebb34e36c822e5f812b4f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-18 00:35:29 -0400

    Time: 4 ms (41.38%) | Memory: 49.1 MB (73.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,41 +1,32 @@
-// Ref: https://leetcode.com/problems/number-of-islands/solutions/56338/java-dfs-and-bfs-solution/comments/278802
 class Solution {
+    // dfs (immediately explore neighbours)
+    // bfs - add to queue, explore neighbours once all in level are added
+    boolean[][] visited;
     public int numIslands(char[][] grid) {
-        if (grid.length == 0)
-            return 0;
-
-        int m = grid.length;
-        int n = grid[0].length;
-
         int count = 0;
-        // boolean[][] visited = new boolean[m][n];
-        Queue<int[]> q = new LinkedList<>();
+        this.visited = new boolean[grid.length][grid[0].length];
+        for (boolean[] row : visited) {
+            Arrays.fill(row, false);
+        }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && visited[i][j] == false) {
                     count++;
-                    q.offer(new int[]{i, j});
-                    // visited[i][j] = true;
-                    BFS(i, j, grid, q);
+                    dfs(i, j, grid);
                 }
             }
         }
+
         return count;
     }
 
-    int[][] dirs = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-    private void BFS(int i, int j, char[][] grid, Queue<int[]> q) {
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            for (int[] dir : dirs) {
-                int x = curr[0] + dir[0], y = curr[1] + dir[1];
-                if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length|| grid[x][y] == '0') {
-                    continue;
-                }
-                grid[x][y] = '0';
-                q.offer(new int[] {x, y});
-            }
-        }
+    public void dfs(int i, int j, char[][] grid) {
+        if (i < 0 || j < 0 || j >= grid[0].length || i >= grid.length || grid[i][j] == '0' || visited[i][j]) return;
+        visited[i][j] = true;
+        dfs(i+1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j-1, grid);
     }
 }
