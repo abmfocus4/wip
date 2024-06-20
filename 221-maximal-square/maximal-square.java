d951a7d731cd086837f54ce0806f5f7385081860
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-20 00:09:35 -0400

    Time: 10 ms (24.48%) | Memory: 57.6 MB (24.32%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,48 +1,40 @@
-// https://www.youtube.com/watch?v=4jBAHd0Xr90&ab_channel=EricProgramming
 class Solution {
-    // top down memo
-    // each cell, starting point for maximal square
-    int maxWidth;
-    Integer[][] cache;
-    int m;
-    int n;
-    char[][] matrix;
-
+    // bottom up
+    // start from i = 1 and j = 1
+    // find top, topDiag, left
     public int maximalSquare(char[][] matrix) {
-        this.matrix = matrix;
-        this.maxWidth = Integer.MIN_VALUE;
-        this.m = matrix.length;
-        this.n = matrix[0].length;
-        this.cache = new Integer[m][n];
-        helper(0, 0);
+        int m = matrix.length, n = matrix[0].length;
+        int[][] cache = new int[m + 1][n + 1]; // i,j maximal square bottom right cell
+
+        // for (int i = 0; i < m; i++) {
+        // if (matrix[i][0] == '1') {
+        // cache[i][0] = 1;
+        // }
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        // if (matrix[0][i] == '1') {
+        // cache[0][i] = 1;
+        // }
+        // }
+
+        int maxWidth = Integer.MIN_VALUE;
+
+        for (int i = 1; i < m + 1; i++) { // m+1 and n+1
+            for (int j = 1; j < n + 1; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    int top = cache[i - 1][j];
+                    int topDiag = cache[i - 1][j - 1];
+                    int left = cache[i][j - 1];
+                    int minWidth = Math.min(top, Math.min(topDiag, left));
+                    cache[i][j] = 1 + minWidth;
+                    maxWidth = Math.max(maxWidth, cache[i][j]);
+
+                }
+            }
+
+        }
+
         return maxWidth == Integer.MIN_VALUE ? 0 : maxWidth * maxWidth;
-    }
-
-    private int helper(int i, int j) {
-        if (i >= m || j >= n)
-            return 0;
-
-        if (cache[i][j] != null) {
-            return cache[i][j];
-        }
-
-        // cur width is 0 then set 0 and exit
-        // if not, then max of all three plus current width
-        // set and return
-
-        int curWidth = matrix[i][j] == '1' ? 1 : 0;
-
-        // todo: for future use
-        int right = helper(i, j + 1);
-        int down = helper(i + 1, j);
-        int rightDiag = helper(i + 1, j + 1);
-
-        if (curWidth == 1) {
-            int minWidth = Math.min(right, Math.min(down, rightDiag)); // min not max
-            curWidth += minWidth;
-        }
-
-        maxWidth = Math.max(curWidth, maxWidth);
-        return cache[i][j] = curWidth;
     }
 }
