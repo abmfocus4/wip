84e8e518b1ba088a5e2c6bad58515f83c87dd6ad
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-22 19:29:44 -0400

    Time: 386 ms (10.75%) | Memory: 79.6 MB (5.73%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,64 +1,27 @@
-// nlogn patience sort
-
-// look at https://www.youtube.com/watch?v=on2hvxBXJH4
-
-// take away: intuition
-
-// look at 
-// https://www.youtube.com/watch?v=i4NBDE8ZEV8&ab_channel=SuyashiSinghal
-
-// take away: elem >= num[i] is replace condition
-
-// --------------------
-
-// for interview:
-
-// {10, 11, 4, 12, 1, 10}
-
-// do a run through like first link
-
-// outline your steps:
-// - Iterate through arr and iterate through list of sub(looked at last elem of each sub to see if we need to append or create new sub)
-
-// - what is time and space complexity of this? discuss?
-
-// - optimize this idea
-// - only store tails since that's all we need
-// - reduce space since you don't need the actual value of LIS coalesce them and only store tails
-// - coalesce condition - use binary search
-// - iterate this is not LIS but representative of stream of data we received
-
-// - pics in phone favorites
 class Solution {
+    int[][] dp;
     public int lengthOfLIS(int[] nums) {
-        int N = nums.length;
-        List<Integer> sub = new ArrayList<>();
-        sub.add(nums[0]);
-        for (int i = 1; i < N; i++) {
-            if (nums[i] > sub.get(sub.size()-1)) { // new size subsequence
-                sub.add(nums[i]);
-            } else { // replace or insert in between
-                int index = binarySearch(nums[i], sub);
-                sub.set(index, nums[i]);
-            }
+        // for every index find the lis
+        int n = nums.length;
+        if (nums.length <= 1) return nums.length;
+        this.dp = new int[n][n+1];
+        for (int[] row : dp) {
+        Arrays.fill(row, -1);
+
         }
-        return sub.size();
+        return lis(nums, 0, -1);
     }
 
-    // checking tail
-    private int binarySearch(int num, List<Integer> sub) {
-        int left = 0;
-        int right = sub.size() - 1;
-        while (left < right) {
-            int mid = (left + right)/2;
-            if (sub.get(mid) == num) {
-                return mid; // replace
-            } else if (sub.get(mid) < num) {
-                left = mid + 1; // insert
-            } else {
-                right = mid;
-            }
+    private int lis(int[] nums, int currentIdx, int prevIdx) {
+        if (currentIdx == nums.length) return 0;
+        if (dp[currentIdx][prevIdx+1] != -1) return dp[currentIdx][prevIdx+1];
+        int skip = lis(nums, currentIdx+1, prevIdx);
+        int select = -1;
+        if (prevIdx == -1 || nums[currentIdx] > nums[prevIdx]) {
+            select = 1 + lis(nums, currentIdx + 1, currentIdx);
         }
-        return left;
+
+        return dp[currentIdx][prevIdx+1] = Math.max(skip, select);
     }
+
 }
