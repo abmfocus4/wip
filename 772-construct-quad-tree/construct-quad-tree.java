21d4a0506cf10d764b7930fb4eeefabdc77fee9f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-23 19:23:12 -0400

    Time: 0 ms (100.00%) | Memory: 44.6 MB (40.43%) - LeetSync

--- 772-construct-quad-tree/construct-quad-tree.java
+++ 772-construct-quad-tree/construct-quad-tree.java
@@ -37,29 +37,31 @@ class Node {
     }
 }
 */
-// https://www.youtube.com/watch?v=tzABQFpl6Mk&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=25&ab_channel=codestorywithMIK
+
 class Solution {
+    int[][] grid;
     public Node construct(int[][] grid) {
-        return solve(grid, 0, 0, grid.length);
+        this.grid = grid;
+        return solve(0,0,grid.length);
     }
 
-    private Node solve(int[][] grid, int x, int y, int n) {
-        if (isSameValue(grid, x, y, n)) {
-            return new Node(grid[x][y] != 0, true); // leaf node
+    public Node solve(int x, int y, int n) {
+        if (isSame(x, y, n)) {
+            return new Node(grid[x][y] != 0, true);
         } else {
-            Node root = new Node(grid[x][y] != 0, false); // simple trick to convert int to boolean
-            root.topLeft = solve(grid, x, y, n/2);
-            root.topRight = solve(grid, x, y+n/2, n/2);
-            root.bottomLeft = solve(grid, x+n/2, y, n/2);
-            root.bottomRight = solve(grid, x+n/2, y+n/2, n/2);
+            Node root = new Node(grid[x][y] != 0, false);
+            root.topLeft = solve(x, y, n/2);
+            root.topRight = solve(x, y+n/2, n/2);
+            root.bottomLeft = solve(x+n/2, y, n/2);
+            root.bottomRight = solve(x+n/2, y+n/2, n/2);
             return root;
         }
     }
 
-    private boolean isSameValue(int[][] grid, int x, int y, int n) {
+    private boolean isSame(int x, int y, int n) {
         int val = grid[x][y];
-        for (int i = x; i < x + n; i++) {
-            for (int j = y; j < y + n; j++) {
+        for (int i = x; i < x+n; i++) {
+            for (int j = y; j < y+n; j++) {
                 if (grid[i][j] != val) {
                     return false;
                 }
