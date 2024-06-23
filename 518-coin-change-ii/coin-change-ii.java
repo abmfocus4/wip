c55db9e911264fd6585c8fa0c475a1b78e97beee
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-23 01:10:22 -0400

    Time: 3 ms (90.31%) | Memory: 40.7 MB (96.56%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,24 +1,14 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[][] dp = new int[n + 1][amount + 1];
-        dp[0][0] = 1; // taking no coins to get amount 0 is 1 way
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
 
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = 1; // making amount 0 is 1 way, don't take coin 
-            for (int j = 1; j <= amount; j++) {
-                // take
-                int take = 0;
-                if (j - coins[i - 1] >= 0) {
-                    take = dp[i][j - coins[i-1]]; // can still use ith coins (unbounded knapsack)
-                }
-
-                // skip
-                int skip = dp[i-1][j]; // don't take ith coin so result is same as i-1 change, and amount doesn't change because you didn't use ith coin
-                dp[i][j] += take+skip;
+        for (int coin : coins) {
+            for (int i = 1; i <= amount; i++) {
+                if (i - coin >= 0)
+                    dp[i] += dp[i - coin];
             }
         }
-
-        return dp[n][amount];
+        return dp[amount];
     }
 }
