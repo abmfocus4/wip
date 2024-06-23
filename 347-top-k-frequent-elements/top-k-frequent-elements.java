55e80783ea6ac773c730d3a0310abfaf2e0e10d2
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-23 01:50:38 -0400

    Time: 12 ms (80.35%) | Memory: 48.4 MB (61.07%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,38 +1,41 @@
-// what elements are repeated the most and top k out of those
-// https://www.youtube.com/watch?v=a3vb_SQVQBo
-
-// Time Complexity: O(n)
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        // create hm of num and freq
-        // create hm of freq and list of nums with same freq
-        // inter N times to get the top k elems
+        // create freq map
+        // sort frequencies in pq min
+        // pop k elems in pq
 
-        Map<Integer, Integer> freqHM = new HashMap<>();
+        // create freq map
+        // reverse index  - key == freq && value == list of elems
+        // iterate from k to 0 and exit at 0 and keep adding elems to return
+
+        HashMap<Integer, Integer> numFreqMap = new HashMap();
         for (int num : nums) {
-            freqHM.put(num, freqHM.getOrDefault(num, 0) + 1);
+            numFreqMap.put(num, numFreqMap.getOrDefault(num, 0) + 1);
         }
 
-        Map<Integer, ArrayList<Integer>> numListHM = new HashMap();
-        for (int num : freqHM.keySet()) {
-            int newListHMKey = freqHM.get(num);
-            if (!numListHM.containsKey(newListHMKey))
-                numListHM.put(newListHMKey, new ArrayList<>());
-            numListHM.get(newListHMKey).add(num);
-        }
-
-        int[] res = new int[k];
-        int N = nums.length;
-        int index = 0;
-        for (int i = N; i > 0; i--) {
-            if (numListHM.containsKey(i)) {
-                ArrayList<Integer> list = numListHM.get(i);
-                for (int listElem : list) {
-                    res[--k] = listElem;
-                    if (k == 0) return res;
-                }
+        HashMap<Integer, List<Integer>> freqNumMap = new HashMap();
+        for (int num : numFreqMap.keySet()) {
+            int freq = numFreqMap.get(num);
+            if (freqNumMap.containsKey(freq) == false) {
+                freqNumMap.put(freq, new ArrayList());
             }
+            freqNumMap.get(freq).add(num);
         }
-        return res;
+
+        int[] result = new int[k];
+        for (int i = nums.length; i >= 0; i--)
+        {
+            if (freqNumMap.containsKey(i)) {
+                List<Integer> list = freqNumMap.get(i);
+                for (int j = 0; j < list.size(); j++) {
+                    result[--k] = list.get(j);
+                    if (k == 0) return result;
+                }
+                
+            }
+            
+        }
+
+        return result;
     }
 }
