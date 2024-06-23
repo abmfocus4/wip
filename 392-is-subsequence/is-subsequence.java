bdffd7118abfa2006aa36df74bbcfa9ae2b21a71
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-23 11:31:52 -0400

    Time: 4 ms (5.79%) | Memory: 41.9 MB (5.26%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,16 +1,34 @@
-// converting s and t to char array and iterating is faster
+// follow up - https://leetcode.com/problems/is-subsequence/solutions/87302/binary-search-solution-for-follow-up-with-detailed-comments
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if (s == null || s.isEmpty()) return true;
-        // s is subsequence of t
-        if (s.length() > t.length()) return false;
-        char[] s_str = s.toCharArray();
-        char[] t_str = t.toCharArray();
-        int j = 0;
+        // create 'character' -> index list map
+        HashMap<Character, List<Integer>> index_map = new HashMap();
         for (int i = 0; i < t.length(); i++) {
-            if (t_str[i] == s_str[j]) j++;
-            if (j == s.length()) return true;
+            char c = t.charAt(i);
+            if (index_map.containsKey(c) == false) {
+                index_map.put(c, new ArrayList());
+            }
+            index_map.get(c).add(i);
         }
-        return false;
+        // iterate through s, and find next index of prevIndex selected through binary search for the current character
+        int prevIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (index_map.containsKey(c) == false)  return false;
+            List<Integer> indexList = index_map.get(c);
+            int j = Collections.binarySearch(indexList, prevIndex);
+
+            if (j < 0) {
+                j = - j - 1; // insertion point of binary search method
+            }
+
+            if (j == indexList.size()) { // matching index does not exist
+                return false;
+            }
+
+            prevIndex = indexList.get(j) + 1; // min next index to find (strictly greater subsequence)
+        }
+
+        return true;
     }
 }
