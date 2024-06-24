56ccb047e65fa64c61bb7b345766b962677f1f57
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-23 22:50:48 -0400

    Time: 2 ms (60.25%) | Memory: 44 MB (86.76%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -9,43 +9,51 @@
  * }
  */
 class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int n1 = 0, n2 = 0;
-        for (ListNode i = l1; i != null; i = i.next) n1++;
-        for (ListNode i = l2; i != null; i = i.next) n2++;
-        
-        Stack<Integer> st = new Stack();
-        int totn = Math.max(n1, n2);
-        for (ListNode i = l1, j = l2; totn > 0; totn--) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack = new Stack();
+        int l1_len = 0, l2_len = 0;
+
+        for (ListNode i = l1; i != null; i = i.next) l1_len++; // find len of both lists
+        for (ListNode i = l2; i != null; i = i.next) l2_len++;
+
+        int total = Math.max(l1_len, l2_len);
+
+        for (ListNode i = l1, j = l2; total > 0; total--) { // total > 0
+            // from top to bottom, just push one number for larger list and 
+            // sum if both a and b are present in l1 and l2 positions
             int a = 0, b = 0;
-            if (totn <= n1) {
+            if (l1_len >= total) {
                 a = i.val;
                 i = i.next;
             }
-            if (totn <= n2) {
+
+            if (l2_len >= total) {
                 b = j.val;
                 j = j.next;
             }
-            st.push(a + b);
+
+            stack.push(a+b);
         }
-        
-        int c = 0;
-        ListNode ans = null;
-        while (!st.empty()) {
-            ListNode i = new ListNode(st.pop());
-            int c1 = (c + i.val) / 10;
-            i.val = (c + i.val) % 10;
-            i.next = ans;
-            ans = i;
-            c = c1;
+
+        ListNode ans = new ListNode(0);
+        int carry = 0;
+        int sum = 0;
+
+        while (stack.isEmpty() == false) { // same as before
+            sum += stack.pop();
+            ans.val = sum % 10; // ans is sum
+            carry = sum / 10;
+            ListNode newNode = new ListNode(carry); // new node is carry
+            newNode.next = ans;
+            ans = newNode;
+            sum = carry;
         }
-        
-        if (c > 0) {
-            ListNode i = new ListNode(c);
-            i.next = ans;
-            ans = i;
+
+        if (carry == 0) {
+            return ans.next;
+        } else {
+            return ans;
         }
-        
-        return ans;
+
     }
 }
