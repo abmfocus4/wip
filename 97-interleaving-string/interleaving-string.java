4026d1caf193749cff4ccb3cfd0a8549a3269b98
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-27 00:52:08 -0400

    Time: 6 ms (32.79%) | Memory: 41.5 MB (77.52%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -10,34 +10,29 @@ class Solution {
         /*
         dp[i][j] -> true if s1[0..i-1] and s2[0..j-1] interleaves s3[0..i+j-1]
         */
-        boolean[][] dp = new boolean[len1+1][len2+1];
+        boolean[] dp = new boolean[len2+1];
         
         /*
         EMPTY s1,s2 can surely interleave to form EMPTY s3
         */
-        dp[0][0] = true;
-        
-        /*
-        If S2 is empty, then just check whether char in s1 matches with that of s3
-        Note: j == 0 here.
-        */
-        for(int i = 1;i<=len1;i++) {
-            dp[i][0] = dp[i-1][0] && s3.charAt(i-1) == s1.charAt(i-1);
-        }
+        dp[0] = true;
         
         /*
         If S1 is empty, then just check whether char in s2 matches with that of s3
         Note: i == 0 here.
         */
         for(int j = 1;j<=len2;j++) {
-            dp[0][j] = dp[0][j-1] && s3.charAt(j-1) == s2.charAt(j-1);
+            dp[j] = dp[j-1] && s3.charAt(j-1) == s2.charAt(j-1);
         }
         
         /*
         Now check for both s1,s2 being non empty
         */
         for(int i = 1; i<=len1; i++) {
+            boolean pre = dp[0];
+            dp[0] = dp[0] && s3.charAt(i-1) == s1.charAt(i-1);
             for(int j = 1; j<=len2; j++) {
+                boolean tmp = dp[j];
                 // Characters to be compared
                 int c1 = s1.charAt(i - 1);
                 int c2 = s2.charAt(j - 1);
@@ -45,10 +40,11 @@ class Solution {
                 
                 // If c3 matches c1, then is every char before c1 in s1 valid?
                 // If c3 matches c2, then is every char before c2 in s2 valid?
-                dp[i][j] = (c1 == c3 && dp[i-1][j]) || (c2 == c3 && dp[i][j-1]);
+                dp[j] = (c1 == c3 && dp[j]) || (c2 == c3 && dp[j-1]);
+                pre = tmp;
             }
         }
         
-        return dp[len1][len2];
+        return dp[len2];
     }
 }
