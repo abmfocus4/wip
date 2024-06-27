582ce5f74e0fdeeb5912c38ca5db2eca4c95aeac
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-26 23:37:25 -0400

    Time: 5 ms (65.52%) | Memory: 44.7 MB (79.46%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,28 +1,31 @@
+// moving indexes and intuition: 
+// https://www.youtube.com/watch?v=XYi2-LPrwm4&ab_channel=NeetCode
+
+// https://www.youtube.com/watch?v=HwDXH35lr0o&ab_channel=NikhilLohia - table for bottom up approach
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
 
-        int[] cur = new int[n+1];
+        int[][] dp = new int[m+1][n+1];
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
 
-        for (int i = 1; i <= n; i++) { // cur[0] = 0;
-            cur[i] = i;
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
 
         for (int i = 1; i <= m; i++) {
-            int pre = cur[0];
-            cur[0] = i; // when word2 is empty
             for (int j = 1; j <= n; j++) {
-                int temp = cur[j];
-                if (word1.charAt(i-1) == word2.charAt(j-1)) {
-                    cur[j] = pre;
+                if (word1.charAt(i-1) == word2.charAt(j-1)) { // account for empty string
+                    dp[i][j] = dp[i-1][j-1];
                 } else {
-                    cur[j] = Math.min(pre, Math.min(cur[j-1], cur[j])) + 1;
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1; // replace, insert, delete + 1 operation
                 }
-                pre = temp;
             }
         }
 
-        return cur[n];
+        return dp[m][n];
     }
 }
