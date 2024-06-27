a6eef99ff6d6420b713d22a616659c7b74347d17
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-26 22:18:09 -0400

    Time: 827 ms (5.01%) | Memory: 48.9 MB (6.06%) - LeetSync

--- 649-dota2-senate/dota2-senate.java
+++ 649-dota2-senate/dota2-senate.java
@@ -1,34 +1,37 @@
-// https://www.youtube.com/watch?v=zZA5KskfMuQ&ab_channel=NeetCodeIO
 class Solution {
     public String predictPartyVictory(String senate) {
-        Queue<Integer> r_q = new LinkedList();
-        Queue<Integer> d_q = new LinkedList();
-        int senate_len = senate.length();
+        int num_r = 0, num_d = 0;
 
-        char[] senate_arr = senate.toCharArray();
+        int n = senate.length();
 
-        for (int i = 0; i < senate_arr.length; i++) {
-            if (senate_arr[i] == 'R') {
-                r_q.add(i);
-            } else {
-                d_q.add(i);
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') num_r++;
+            else num_d++;
+        }
+
+        Set<Integer> bannedIdx = new HashSet();
+
+        while (num_r != 0 && num_d != 0) {
+            for (int i = 0; i < n; i++) {
+                if (num_r == 0 || num_d == 0) break;
+                if (bannedIdx.contains(i)) continue;
+                int j = (i + 1)%n;
+                while (j != i) {
+                    if (bannedIdx.contains(j) == false && senate.charAt(i) != senate.charAt(j)) {
+                        bannedIdx.add(j);
+                        if (senate.charAt(j) == 'R') num_r--;
+                        else num_d--;
+                        break;
+                    }
+                    j = (j+1)%n;
+                }
             }
         }
 
-        while (r_q.isEmpty() == false && d_q.isEmpty() == false) {
-            int r = r_q.poll(); // greedy: kick out nearest neighbour
-            int d = d_q.poll();
-            if (r < d) { // r comes before d
-                r_q.add(r + senate_len); // kick d out and push r into circulation at the end
-            } else {
-                d_q.add(d + senate_len);
-            }
-        }
-
-        if (r_q.size() > d_q.size()) {
-            return "Radiant";
-        } else {
+        if (num_r == 0) {
             return "Dire";
+        } else {
+            return "Radiant";
         }
     }
 }
