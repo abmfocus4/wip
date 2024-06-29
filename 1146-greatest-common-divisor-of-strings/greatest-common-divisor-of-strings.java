2c8b463c80ed8bbf8d4d70eedc5d5917579bd074
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-28 23:40:03 -0400

    Time: 1 ms (74.49%) | Memory: 42.1 MB (35.84%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,20 +1,15 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        for (int i = Math.min(str1.length(), str2.length()); i >= 1; i--) {
-            if (isBase(str1, str2, i)) {
-                return str1.substring(0, i);
-            }
+        if ((str1 + str2).equals(str2 + str1) == false) {
+            return "";
         }
 
-        return "";
+        int gcd = getGcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
     }
 
-    private boolean isBase(String str1, String str2, int k) {
-        if (str1.length() % k > 0 || str2.length() % k > 0) {
-            return false;
-        } else {
-            String base = str1.substring(0, k);
-            return str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty();
-        }
+    public int getGcd(int p, int q) {
+        if (q == 0) return p;
+        return getGcd(q, p % q);
     }
 }
