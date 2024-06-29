52d889dea6cb69a951318f48883f9cc85ff5ca55
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-29 14:29:57 -0400

    Time: 0 ms (100.00%) | Memory: 40.3 MB (31.19%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,26 +1,8 @@
+// https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/solutions/479998/c-bitwise-xor-solution-1-line
 class Solution {
     public int minFlips(int a, int b, int c) {
-        int flip = 0;
-        while(a != 0 || b != 0 || c != 0) {
-            if ((c & 1) == 0) {
-                if ((a & 1) == 1) {
-                    flip++;
-                }
-
-                if ((b & 1) == 1) {
-                    flip++;
-                }
-            } else { // to be 1
-                if ((a & 1) == 0 && (b & 1) == 0) {
-                    flip++;
-                }
-
-            }
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
-
-        return flip;
+        int firstCase  = (c ^ (a|b));
+        int secondCase = firstCase & (a&b); // case where a and b is 1 and c is 0
+        return Integer.bitCount(firstCase) + Integer.bitCount(secondCase);
     }
 }
