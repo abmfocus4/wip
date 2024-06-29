34a74222f813ef6268c2bd622367a3afb130fe1a
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-29 11:45:32 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (46.52%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -1,36 +1,37 @@
-// https://www.youtube.com/watch?v=qSbJZWENtX4&ab_channel=NickWhite
-// lexicographically as in dictionary - check order of words
 class Solution {
-    int[] char_map;
+    int[] index_map;
     public boolean isAlienSorted(String[] words, String order) {
-        this.char_map = new int[26];
+        this.index_map = new int[26];
+
         for (int i = 0; i < order.length(); i++) {
-            char_map[order.charAt(i) - 'a'] = i; // map character to index of character
+            index_map[order.charAt(i) - 'a'] = i; 
         }
 
         for (int i = 1; i < words.length; i++) {
-            if (compare(words[i-1], words[i]) > 0) { // has to be less than or equal to 0
+            if (diff(words[i - 1], words[i]) > 0) {
                 return false;
             }
         }
+
         return true;
     }
 
-    public int compare(String word1, String word2) {
-        int compare_val = 0;
+    private int diff(String word1, String word2) {
         int i = 0;
         int j = 0;
-        
-        while (i < word1.length() && j < word2.length() && compare_val == 0) {
-            compare_val = char_map[word1.charAt(i) - 'a'] - char_map[word2.charAt(j) - 'a'];
+
+        int diff = 0;
+
+        while (i < word1.length() && j < word2.length() && diff == 0) {
+            diff = index_map[word1.charAt(i) - 'a'] - index_map[word2.charAt(i) - 'a'];
             i++;
             j++;
         }
 
-        if (compare_val == 0) { // hell, hello
+        if (diff == 0) {
             return word1.length() - word2.length();
         } else {
-            return compare_val;
+            return diff;
         }
     }
 }
