540ae772d82658538632f73e58467d063ce9350d
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-29 20:56:51 -0400

    Time: 44 ms (23.90%) | Memory: 58.4 MB (45.82%) - LeetSync

--- 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.java
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.java
@@ -1,31 +1,29 @@
 class Solution {
     public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
-        // same as this problem (with small diff of atmost k elements)- https://leetcode.com/problems/maximum-subsequence-score/submissions/1253164216/?envType=study-plan-v2&envId=leetcode-75
-        
-        // create pairs of corresponding speed and efficiency indexes
-        // sort pairs by efficiency in descending order
-        // create min heap to keep track of k elements used to calculate performance
-        
-        int[][] pairs = new int[n][2];
-
+        // sum of speed * min of efficiency
+        // min in window is chosen as efficiency
+        int[][] team = new int[n][2];
         for (int i = 0; i < n; i++) {
-            pairs[i] = new int[] {speed[i], efficiency[i]};
+            team[i] = new int[] {speed[i], efficiency[i]};
         }
+        Arrays.sort(team, (a,b) -> b[1] - a[1]); // sort by efficiency big to small
 
-        Arrays.sort(pairs, (a,b)->b[1] - a[1]);
-
-        Queue<Integer> minHeap = new PriorityQueue(k+1);
+        // slide window by popping min speed 
+        PriorityQueue<Integer> q = new PriorityQueue<>(k+1);
 
         long maxPerf = 0;
-        long curPerf = 0;
-        for (int[] pair : pairs) {
-            minHeap.add(pair[0]);
-            curPerf += pair[0];
-            if (minHeap.size() > k) curPerf -= minHeap.poll();
-            maxPerf = Math.max(maxPerf, curPerf * pair[1]); // DIFF FROM OTHER PROBLEM (NO K CHECK)
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += team[i][0];
+            q.add(team[i][0]);
+            if (q.size() > k) {
+                sum -= q.poll();
+            }
+            maxPerf = Math.max(maxPerf, sum * team[i][1]);
+            
         }
 
         return (int) (maxPerf % (long)(1e9 + 7));
-
     }
 }
