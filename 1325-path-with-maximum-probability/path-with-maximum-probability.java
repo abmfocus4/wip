c4e643197bac55cada570c6d13e2e5d628c33899
Meg Alapati <bmalapat@uwaterloo.ca>
2024-06-30 19:30:53 -0400

    Time: 46 ms (46.87%) | Memory: 58.8 MB (12.53%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,45 +1,35 @@
 class Solution {
-    
-  private record Node(int node, double dist) {
-  }
-
-  public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-    Map<Integer, List<Node>> graph = new HashMap<>();
-    for (int i = 0; i < edges.length; i++) {
-      int u = edges[i][0];
-      int v = edges[i][1];
-      double minusLog = -Math.log(succProb[i]);
-      graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new Node(v, minusLog));
-      graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new Node(u, minusLog));
-    }
-
-    double[] dist = new double[n];
-    Arrays.fill(dist, -1.0);
-
-    PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> a.dist));
-    pq.offer(new Node(start, 0.0));
-
-    Set<Integer> visited = new HashSet<>();
-    visited.add(start);
-
-    while (!pq.isEmpty()) {
-      Node cur = pq.poll();
-      if (dist[cur.node] != -1.0) {
-        continue;
-      }
-      if (cur.dist > dist[cur.node]) {
-        dist[cur.node] = cur.dist;
-      }
-      if (cur.node == end) {
-        return Math.exp(-dist[cur.node]);
-      }
-      visited.add(cur.node);
-      for (Node next : graph.getOrDefault(cur.node, List.of())) {
-        if (!visited.contains(next.node)) {
-          pq.offer(new Node(next.node, dist[cur.node] + next.dist));
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        Map<Integer, List<Pair<Integer, Double>>> graph = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0], v = edges[i][1];
+            double pathProb = succProb[i];
+            graph.computeIfAbsent(u, k -> new ArrayList<>()).add(new Pair<>(v, pathProb));
+            graph.computeIfAbsent(v, k -> new ArrayList<>()).add(new Pair<>(u, pathProb));
         }
-      }
+
+        double[] maxProb = new double[n];
+        maxProb[start] = 1d;
+
+        PriorityQueue<Pair<Double, Integer>> pq = new PriorityQueue<>((a, b) -> -Double.compare(a.getKey(), b.getKey()));
+        pq.add(new Pair<>(1.0, start));
+        while (!pq.isEmpty()) {
+            Pair<Double, Integer> cur = pq.poll();
+            double curProb = cur.getKey();
+            int curNode = cur.getValue();
+            if (curNode == end) {
+                return curProb;
+            }
+            for (Pair<Integer, Double> nxt : graph.getOrDefault(curNode, new ArrayList<>())) {
+                int nxtNode = nxt.getKey();
+                double pathProb = nxt.getValue();
+                if (curProb * pathProb > maxProb[nxtNode]) {
+                    maxProb[nxtNode] = curProb * pathProb;
+                    pq.add(new Pair<>(maxProb[nxtNode], nxtNode));
+                }
+            }
+        }
+
+        return 0d;
     }
-    return 0.0;
-  }
 }
