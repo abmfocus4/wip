9a0897e6be208e8c3f90f5d0fbdfced9397d4816
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-01 18:40:03 -0400

    Time: 1 ms (75.39%) | Memory: 41.1 MB (90.10%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,7 +3,7 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 0; j < strs.length; j++) {
+            for (int j = 1; j < strs.length; j++) {
                 if (i > strs[j].length() - 1 || strs[0].charAt(i) != strs[j].charAt(i)) {
                     return sb.toString();
                 }
