56a9bf12579d5cec593bba80f73945efce39f157
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-02 21:03:56 -0400

    Time: 0 ms (100.00%) | Memory: 44.6 MB (65.71%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,12 +1,10 @@
 class Solution {
-    // brute force
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         for (int i = 1; i < n; i++) {
             for (int j = i; j < n; j++) {
-                int sum = nums[j] + nums[j-i];
-                if (sum == target) {
-                    return new int[] {j, j-i};
+                if (nums[j] + nums[j-i] == target) {
+                    return new int[] {j, j - i};
                 }
             }
         }
