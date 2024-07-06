3f06e8d3dd41a68a821e344252a1f54ee9c79ad0
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-06 19:35:55 -0400

    Time: 17 ms (54.81%) | Memory: 44.9 MB (51.70%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,50 +1,48 @@
 class Solution {
+    // Sliding Window problem
+    // Time: O(N+N+M) or Time: O(N+M) 
     public String minWindow(String s, String t) {
-        // frequency map
-        // start, count, minLength, minStart
-
-        if (s == null || t == null || s.isEmpty() || t.isEmpty() || s.length() < t.length())
-            return "";
-
+        if (s == null || t == null || s.isEmpty() || t.isEmpty() || s.length() < t.length()) return "";
+        
+        // create map for t str
+        // Space: O(M)
         HashMap<Character, Integer> map = new HashMap();
         for (char c : t.toCharArray()) {
             if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
+                map.put(c, map.get(c)+1);
             } else {
                 map.put(c, 1);
             }
         }
 
-        int start = 0;
-        int count = 0;
-        int minStart = 0;
-        int minLength = Integer.MAX_VALUE;
+        int count = 0; // window eligibility check
+        int start = 0; // left boundary pointer
 
-        for (int i = 0; i < s.length(); i++) {
+        // store result
+        int minLength = Integer.MAX_VALUE; // best smallest substring size
+        int minLeft = 0; // best left boundary
+
+        for (int i = 0; i < s.length(); i++) { // right boundary managed by loop
             if (map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
-                if (map.get(s.charAt(i)) >= 0)
-                    count++; // err
+                map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                if (map.get(s.charAt(i)) >= 0) count++;
             }
 
-            while (count == t.length()) {
+            while (count == t.length()) { // found eligible window
                 if (minLength > i - start + 1) {
                     minLength = i - start + 1;
-                    minStart = start;
+                    minLeft = start;
                 }
 
-                if (map.containsKey(s.charAt(start))) {
-                    map.put(s.charAt(start), map.get(s.charAt(start)) + 1);
-                    if (map.get(s.charAt(start)) > 0)
-                        count--;
+                if (map.containsKey(s.charAt(start))) { // shrink window
+                    map.put(s.charAt(start), map.get(s.charAt(start))+1);
+                    if (map.get(s.charAt(start)) > 0) count--;
                 }
                 start++;
             }
         }
 
-        if (s.length() < minLength) //err
-            return "";
-        return s.substring(minStart, minStart + minLength);
-
+        if (minLength > s.length()) return "";
+        return s.substring(minLeft, minLeft + minLength);
     }
 }
