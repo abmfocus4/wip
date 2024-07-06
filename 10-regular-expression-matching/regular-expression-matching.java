db2d36d920b78d095380a8610ea1b62ecc04f0de
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-06 10:10:21 -0400

    Time: 1 ms (100.00%) | Memory: 42.8 MB (16.28%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -4,7 +4,10 @@ class Solution {
         if(text == null && pattern == null) return true;
         if(text == null || pattern == null) return false;
 
-        return isMatch(0, 0, text, pattern);
+        // + 1 to account for empty string case
+        Boolean[][] memo = new Boolean[text.length() + 1][pattern.length() + 1];
+
+        return isMatch(0, 0, text, pattern, memo);
         // possible scenarios
         
         // 1) char match with .
@@ -12,22 +15,27 @@ class Solution {
         // 3) c*/.*
     }
 
-    private boolean isMatch(int i, int j, String text, String pattern) {
-    // finished matching all characters, both need to go out of bound
+    private boolean isMatch(int i, int j, String text, String pattern, Boolean[][] memo) {
+    // finished matching all characters, both need to go out of bound        
         if (j == pattern.length()) {
             return i == text.length();
+        }
+
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
 
         boolean isFirstCharMatch = (i < text.length() &&
                 (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
 
-
+        boolean hasMatched = false;
         if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
-            boolean repeatMultiple = isFirstCharMatch && isMatch(i + 1, j, text, pattern); // repeat multiple
-            boolean repeatZero = isMatch(i, j + 2, text, pattern); // repeat 0
-            return repeatMultiple || repeatZero;
+            boolean repeatMultiple = isFirstCharMatch && isMatch(i + 1, j, text, pattern, memo); // repeat multiple
+            boolean repeatZero = isMatch(i, j + 2, text, pattern, memo); // repeat 0
+            hasMatched = repeatMultiple || repeatZero;
         } else {
-            return isFirstCharMatch && isMatch(i + 1, j + 1, text, pattern);
+            hasMatched = isFirstCharMatch && isMatch(i + 1, j + 1, text, pattern, memo);
         }
+        return memo[i][j] = hasMatched;
     }
 }
