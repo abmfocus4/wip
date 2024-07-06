6fd18e98b6bba4421791e5375ec43ebfd6243e53
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-06 10:27:28 -0400

    Time: 2 ms (64.41%) | Memory: 42 MB (40.59%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,41 +1,24 @@
-// https://www.youtube.com/watch?v=3vbBrl-LeDc - Brute Force video
 class Solution {
     public boolean isMatch(String text, String pattern) {
-        if(text == null && pattern == null) return true;
-        if(text == null || pattern == null) return false;
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        // dp(i, j): does text[i:] and pattern[j:] match
+        dp[text.length()][pattern.length()] = true;
 
-        // + 1 to account for empty string case
-        Boolean[][] memo = new Boolean[text.length() + 1][pattern.length() + 1];
+        for (int i = text.length(); i >= 0; i--) {
+            for (int j = pattern.length() - 1; j >= 0; j--) {
+                boolean first_match =
+                    (i < text.length() && (pattern.charAt(j) == text.charAt(i) ||
+                            pattern.charAt(j) == '.'));
 
-        return isMatch(0, 0, text, pattern, memo);
-        // possible scenarios
-        
-        // 1) char match with .
-        // 2) char match with char
-        // 3) c*/.*
-    }
-
-    private boolean isMatch(int i, int j, String text, String pattern, Boolean[][] memo) {
-    // finished matching all characters, both need to go out of bound        
-        if (j == pattern.length()) {
-            return i == text.length();
+                if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
+                    boolean notTake = dp[i][j + 2];
+                    boolean take = (first_match && dp[i + 1][j]);
+                    dp[i][j] =  notTake || take;
+                } else {
+                    dp[i][j] = first_match && dp[i + 1][j + 1];
+                }
+            }
         }
-
-        if (memo[i][j] != null) {
-            return memo[i][j];
-        }
-
-        boolean isFirstCharMatch = (i < text.length() &&
-                (pattern.charAt(j) == text.charAt(i) || pattern.charAt(j) == '.'));
-
-        boolean hasMatched = false;
-        if (j + 1 < pattern.length() && pattern.charAt(j + 1) == '*') {
-            boolean repeatMultiple = isFirstCharMatch && isMatch(i + 1, j, text, pattern, memo); // repeat multiple
-            boolean repeatZero = isMatch(i, j + 2, text, pattern, memo); // repeat 0
-            hasMatched = repeatMultiple || repeatZero;
-        } else {
-            hasMatched = isFirstCharMatch && isMatch(i + 1, j + 1, text, pattern, memo);
-        }
-        return memo[i][j] = hasMatched;
+        return dp[0][0];
     }
 }
