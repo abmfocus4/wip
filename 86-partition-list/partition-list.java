a60d9865c3b90ce525f7b20b506cb3dca731c4fc
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-07 12:55:20 -0400

    Time: 0 ms (100.00%) | Memory: 42 MB (29.60%) - LeetSync

--- 86-partition-list/partition-list.java
+++ 86-partition-list/partition-list.java
@@ -1,31 +1,35 @@
-// https://www.youtube.com/watch?v=NYZKRUT_8MY&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=16&ab_channel=codestorywithMIK
-//Time : O(n), we visit each node only once
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode small = new ListNode(0);
-        ListNode large = new ListNode(0);
-        
-        ListNode smallP = small;
-        ListNode largeP = large;
-        
-        while(head != null) {
-            
-            if(head.val < x) {
-                smallP.next = head;
-                smallP = smallP.next;
+        ListNode lesserLL = new ListNode(0);
+        ListNode greaterLL = new ListNode(0);
+
+        ListNode lesserHead = lesserLL;
+        ListNode greaterHead = greaterLL;
+
+        while (head != null) {
+            if (head.val < x) {
+                lesserLL.next = head;
+                lesserLL = lesserLL.next;
             } else {
-                largeP.next = head;
-                largeP = largeP.next;
+                greaterLL.next = head;
+                greaterLL = greaterLL.next;
             }
-            
             head = head.next;
-            
         }
-        
-        
-        smallP.next = large.next;
-        largeP.next = null;
-        
-        return small.next;
+
+        lesserLL.next = greaterHead.next;
+        greaterLL.next = null;
+
+        return lesserHead.next;
     }
 }
