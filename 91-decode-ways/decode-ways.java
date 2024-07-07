f3ec62fc4bded79c05176055904262716cb00c7d
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-07 13:51:21 -0400

    Time: 1 ms (79.37%) | Memory: 41.6 MB (53.28%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,20 +1,30 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s.length() == 0 || s.charAt(0) == '0') return 0;
-
-        if (s.length() == 1) return 1;
-
-        int count1 = 1, count2 = 1;
-        for (int i = 1; i < s.length(); i++) {
-            int d = s.charAt(i) - '0';
-            int dd = d + (s.charAt(i-1) - '0')*10;
-            int count = 0;
-            if (d > 0) count += count1;
-            if (10 <= dd && dd <= 26) count += count2;
-            count2 = count1;
-            count1 = count;
+        if (s.length() == 0 || s.charAt(0) == '0') {
+            return 0;
         }
 
-        return count1;
+        if (s.length() == 1) {
+            return 1;
+        }
+
+        int count_prev = 1, count_prev_prev = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int d = s.charAt(i) - '0';
+            int dd = (s.charAt(i-1) - '0')*10 + d;
+            int count = 0;
+            if (d > 0) {
+                count += count_prev;
+            }
+            if (dd >= 10 && dd <= 26) {
+                count += count_prev_prev;
+            }
+
+            count_prev_prev = count_prev;
+            count_prev = count;
+        }
+
+        return count_prev;
     }
 }
