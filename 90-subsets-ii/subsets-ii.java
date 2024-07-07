d963c362d5251bf57b68e50b97ed235a10860d86
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-07 13:33:44 -0400

    Time: 2 ms (38.00%) | Memory: 43.8 MB (7.83%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,19 +1,21 @@
-// https://leetcode.com/problems/permutations/solutions/18239/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, 0);
-    return list;
-}
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
+        List<List<Integer>> res = new ArrayList();
+        Arrays.sort(nums);
+        backtrack(nums, res, new ArrayList(), 0);
+        return res;
     }
-} 
+
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> subset, int start) {
+        res.add(new ArrayList(subset));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i - 1 >= start && nums[i] == nums[i-1]) continue;
+            // if ((i-1 >= start) && nums[i] != nums[i - 1]) {
+                subset.add(nums[i]);
+                backtrack(nums, res, subset, i + 1);
+                subset.remove(subset.size() - 1);
+            // }
+        }
+    }
 }
