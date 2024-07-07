c5ea95590bbbe9ab0ea6abf335b90022f37d4f89
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-07 16:44:30 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (28.06%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,43 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) return true;
-        if (root1 == null || root2 == null) return false;
-        TreeNode c1 = root1, c2 = root2;
-        while (c1 != null && c2 != null) {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null ^ q == null) {
+            return false;
+        }
 
-            if (c1.val != c2.val) return false;
+        TreeNode pCur = p;
+        TreeNode qCur = q;
 
-            TreeNode pre1 = c1.left, pre2 = c2.left;
-
-            while (pre1 != null && pre1.right != null && pre1.right != c1) {
-                pre1 = pre1.right;
+        while (pCur != null && qCur != null) {
+            if (pCur.val != qCur.val) {
+                return false;
             }
-            while (pre2 != null && pre2.right != null && pre2.right != c2) {
-                pre2 = pre2.right;
+            TreeNode pPrev = pCur.left, qPrev = qCur.left;
+
+            while (pPrev != null && pPrev.right != null && pPrev.right != pCur) {
+                pPrev = pPrev.right;
             }
-            
-            if (pre1 == null ^ pre2 == null) return false;
-            
-            if (pre1 == null) { // if left is null
-                c1 = c1.right;
-            } else if (pre1.right == null) {
-                pre1.right = c1;
-                c1 = c1.left;
+
+            while (qPrev != null && qPrev.right != null && qPrev.right != qCur) {
+                qPrev = qPrev.right;
+            }
+
+            if (pPrev == null ^ qPrev == null) return false;
+
+            if (pPrev == null) {
+                pCur = pCur.right;
+            } else if (pPrev.right == null) {
+                pPrev.right = pCur;
+                pCur = pCur.left;
             } else {
-                pre1.right = null;
-                c1 = c1.right;
+                pPrev.right = null;
+                pCur = pCur.right;
             }
-            
-            if (pre2 == null) {
-                c2 = c2.right;
-            } else if (pre2.right == null) {
-                pre2.right = c2;
-                c2 = c2.left;
+
+            if (qPrev == null) {
+                qCur = qCur.right;
+            } else if (qPrev.right == null) {
+                qPrev.right = qCur;
+                qCur = qCur.left;
             } else {
-                pre2.right = null;
-                c2 = c2.right;
+                qPrev.right = null;
+                qCur = qCur.right;
             }
         }
-        return c1 == null && c2 == null;
+
+        return pCur == null && qCur == null;
+
+
     }
 }
