1b844ca21e49f12aae942bce939e82dfbb63fad3
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-06 20:14:49 -0400

    Time: 0 ms (100.00%) | Memory: 41.5 MB (65.66%) - LeetSync

--- 68-text-justification/text-justification.java
+++ 68-text-justification/text-justification.java
@@ -1,68 +1,63 @@
-// https://www.youtube.com/watch?v=jpU2LVaDa4g&ab_channel=codestorywithMIK
 class Solution {
-    int MAX_WIDTH;
-    
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> lines = new ArrayList<>();
-        int n     = words.length;
-        this.MAX_WIDTH = maxWidth;
-        int i     = 0, j = 0;
+        List<String> lines = new ArrayList();
+        int n = words.length;
+        int i = 0;
+        int j = 0;
+        while (i < n) {
+            int lettersSlots = words[i].length();
+            int spaceSlots = 0;
+            j = i+1;
 
-        int spaceBetweenWords = 0, extraSpacesToDistribute = 0;
-        int lettersCount = 0, spaceSlots = 0;
-        int remainingSlots = 0;
-        
-        while(i < n) {
-            lettersCount = words[i].length();
-            j            = i+1;
-            spaceSlots   = 0;
-            
-            while(j < n && (spaceSlots + lettersCount + words[j].length() + 1 <= maxWidth)) { // keep adding words to line
-                lettersCount += words[j].length();
-                spaceSlots   += 1;
+            while ((j < n) && lettersSlots + spaceSlots + words[j].length() + 1 <= maxWidth) {
+                lettersSlots += words[j].length();
+                spaceSlots++;
                 j++;
             }
-            
-            remainingSlots = maxWidth - lettersCount; // remaining slots in line
-            
-            if (j == n) { // if the line we just processed is the last line
-                spaceBetweenWords = 1;
-                extraSpacesToDistribute    = 0;
+
+            int remainingSlots = maxWidth - lettersSlots;
+            int spacesBetweenWords = 0;
+            int spacesToDistribute = 0;
+            if (j == n) {
+                spacesBetweenWords = 1;
+                spacesToDistribute = 0;
             } else {
-                spaceBetweenWords = spaceSlots == 0 ? 0 : remainingSlots / spaceSlots; // evenly divide remaining spaces
-                extraSpacesToDistribute = spaceSlots == 0 ? 0 : remainingSlots % spaceSlots; // extra spaces after even division
+                spacesBetweenWords = spaceSlots == 0 ? 0 : remainingSlots/spaceSlots;
+                spacesToDistribute = spaceSlots == 0 ? 0 : remainingSlots%spaceSlots;
             }
+
+            lines.add(getLine(i, j, spacesBetweenWords, spacesToDistribute, words, maxWidth));
             
-            lines.add(getLine(i, j, spaceBetweenWords, extraSpacesToDistribute, words));
-            i = j; // process next line
+            i = j;
         }
-        
+
         return lines;
     }
-    
-    private String getLine(int i, int j, int spaceBetweenWords, int extraSpacesToDistribute, String[] words) {
-        StringBuilder s = new StringBuilder();
 
-        for(int k = i; k < j; k++) {
-            s.append(words[k]);
+    private String getLine(int firstIdx, int lastIdx, int spacesBetweenWords, int spacesToDistribute, String[] words, int maxWidth) {
+        StringBuilder sb = new StringBuilder();
+        for (int k = firstIdx; k < lastIdx; k++) 
+        {
+            sb.append(words[k]);
 
-            if(k == j-1) // if it is last line, don't add spaces or do anything fancy
+            if (k == lastIdx - 1) {
                 break;
+            }
 
-            for(int space = 1; space <= spaceBetweenWords; space++)
-                s.append(" ");
+            for (int space = 1; space <= spacesBetweenWords; space++) {
+                sb.append(" ");
+            }
 
-            if(extraSpacesToDistribute > 0) { // distribute extra spaces from left to right 1 each
-                s.append(" ");
-                extraSpacesToDistribute--;
+            if (spacesToDistribute > 0) {
+                sb.append(" ");
+                spacesToDistribute--;
             }
         }
 
-        while(s.length() < MAX_WIDTH) { // pad with extra spaces
-            s.append(" ");
+        while (sb.length() < maxWidth) {
+            sb.append(" ");
         }
-        
-        return s.toString();
+
+        return sb.toString();
     }
-    
 }
