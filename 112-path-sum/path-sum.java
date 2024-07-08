27179d78bc98186faec43104f0fb31565edb0817
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-07 22:20:10 -0400

    Time: 0 ms (100.00%) | Memory: 42.9 MB (63.67%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,23 +1,48 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-//  https://www.youtube.com/watch?v=LSKQyOz_P8I&ab_channel=NeetCode
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root == null) return false;
-        if(root.left == null && root.right == null) return (targetSum - root.val) == 0;
-        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+        if (root == null)
+            return false;
+        TreeNode prev, cur = root;
+        int curSum = 0;
+        while (cur != null) {
+            if (cur.left == null) {
+                curSum += cur.val;
+                if (cur.right == null && curSum == targetSum) {
+                    return true;
+                }
+                cur = cur.right;
+
+            } else {
+                prev = cur.left;
+                int sum = prev.val;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                    sum += prev.val;
+                }
+                if (prev.right == cur) {
+                    prev.right = null;
+                    cur = cur.right;
+                    curSum -= sum;
+                } else { // prev.right == null
+                    curSum += cur.val;
+                    prev.right = cur;
+                    if (prev.left == null && curSum + sum == targetSum) {
+                        return true;
+                    }
+                    cur = cur.left;
+                }
+            }
+        }
+        return false;
     }
 }
