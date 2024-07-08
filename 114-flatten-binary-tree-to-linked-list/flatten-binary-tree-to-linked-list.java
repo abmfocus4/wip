ca85141aedd94c96c398db1372f784be48ae3076
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-08 14:09:53 -0400

    Time: 0 ms (100.00%) | Memory: 41.9 MB (68.30%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,34 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-// https://www.youtube.com/watch?v=sWf7k1x9XR4&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=19&ab_channel=takeUforward
 class Solution {
     public void flatten(TreeNode root) {
-        TreeNode cur = root;
-        while (cur != null) {
-            if (cur.left != null) {
-                TreeNode prev = cur.left;
-                while (prev.right != null) { // find right most node
+        if (root == null) {
+            return;
+        }
+
+        while(root != null) {
+            if (root.left != null) {
+                TreeNode prev = root.left;
+                while (prev.right != null) {
                     prev = prev.right;
                 }
-                prev.right = cur.right; // threaded binary tree // figure out where the right most left subtree node should point
-
-                cur.right = cur.left;
-                cur.left = null;
+                prev.right = root.right;
+                root.right = root.left;
+                root.left = null;
             }
-            cur = cur.right;
+            root = root.right;
         }
     }
 }
