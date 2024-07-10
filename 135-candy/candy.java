1b02d3060ab083862f3006940bb0a1661b4f10c1
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-09 22:15:01 -0400

    Time: 2 ms (99.89%) | Memory: 45.9 MB (14.19%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,26 +1,33 @@
-// O(n) space - brute force
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] count = new int[n]; // l2r and r2l count store
-        Arrays.fill(count, 1);
-
-        // move l2r and find candies
-        for (int i = 1; i < n; i++)  {
-            if (ratings[i] > ratings[i-1]) {
-                // should have more candies
-                count[i] = Math.max(count[i], count[i-1] + 1); // more than the neighbour with low ratings
+        int total = n;
+        int i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
             }
-        }
 
-        /// move r2l and store candies
-        for (int i = n-2; i >= 0; i--) {
-            if (ratings[i] > ratings[i+1]) {
-                // should have more candies
-                count[i] = Math.max(count[i], count[i+1] + 1); // more than the neighbour with low ratings
+            int peak = 0;
+            while (ratings[i] > ratings[i - 1]) {
+                peak++;
+                total += peak;
+                i++;
+                if (i == n) {
+                    return total;
+                }
             }
-        }
 
-        return Arrays.stream(count).sum();
+            int dip = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                dip++;
+                total += dip;
+                i++;
+            }
+
+            total -= Math.min(dip, peak);
+        }
+        return total;
     }
 }
