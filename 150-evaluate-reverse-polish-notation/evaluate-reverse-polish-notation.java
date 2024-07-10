54a90a123a0278fc98850cc870185579fec7e167
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-10 11:04:52 -0400

    Time: 5 ms (97.04%) | Memory: 44.2 MB (90.45%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,37 +1,35 @@
-// https://www.youtube.com/watch?v=BM-bu4tJz7U&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=6&ab_channel=codestorywithMIK
 class Solution {
-
-    private int Operator(String op, int a, int b) {
-        if (op.equals("+")) {
-            return a + b;
-        } else if (op.equals("-")) {
-            return a - b;
-        } else if (op.equals("*")) {
-            return a * b;
-        } else if (op.equals("/")) {
-            return (a / b);
-        }
-
-        return -1;
-    }
-
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack();
 
         for (String token : tokens) {
-            if (token.equals("+") || token.equals("*") || token.equals("-") ||
-                    token.equals("/")) {
-
-                // pop b and then a (not a and b since div is not reflective)
+            if (isOperator(token)) {
                 int b = stack.pop();
                 int a = stack.pop();
-                int result = Operator(token, a, b);
+
+                int result = operate(token, a, b);
                 stack.push(result);
             } else {
-                stack.push(Integer.parseInt(token)); // convert to int
+                stack.push(Integer.parseInt(token));
             }
         }
 
         return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int operate(String operator, int a, int b) {
+        if (operator.equals("+")) {
+            return a + b;
+        } else if (operator.equals("-")) {
+            return a - b;
+        } else if (operator.equals("/")) {
+            return a/b;
+        } else {
+            return a*b;
+        }
     }
 }
