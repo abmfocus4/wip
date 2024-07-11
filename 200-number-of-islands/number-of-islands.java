a8404e69b5154285927b5ece6e777061c025838f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-11 13:43:10 -0400

    Time: 2 ms (99.49%) | Memory: 49.3 MB (64.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,42 +1,37 @@
 class Solution {
-    // dfs (immediately explore neighbours)
-    // bfs - add to queue, explore neighbours once all in level are added
-    boolean[][] visited;
-    Queue<int[]> q;
     public int numIslands(char[][] grid) {
+        // if we find a 1 it's a island
+        // start from cell == 1 in the grid
+        // run bfs or dfs to convert as many connected 1s to 0s
+
+
+        int m = grid.length;
+        int n = grid[0].length;
         int count = 0;
-        this.visited = new boolean[grid.length][grid[0].length];
-        for (boolean[] row : visited) {
-            Arrays.fill(row, false);
-        }
 
-        this.q = new LinkedList();
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    q.add(new int[] {i, j});
-                    grid[i][j] = '0';
-                    bfs(i, j, grid);
+                    dfs(i, j, grid);
                 }
             }
         }
 
         return count;
+
     }
-    int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
-    public void bfs(int i, int j, char[][] grid) {
-        while (q.isEmpty() == false) {
-            int[] coord = q.poll();
-            for (int[] dir : dirs) {
-                int x = coord[0] + dir[0];
-                int y = coord[1] + dir[1];
-                if (x < 0 || y < 0 || y >= grid[0].length || x >= grid.length || grid[x][y] == '0') continue;
-                q.add(new int[] {x, y});
-                grid[x][y] = '0';
-            }
+
+    private void dfs(int row, int col, char[][] grid) {
+        // do some boundary and visited checks
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0') {
+            return;
         }
 
+        grid[row][col] = '0';
+        dfs(row + 1, col, grid);
+        dfs(row - 1, col, grid);
+        dfs(row, col + 1, grid);
+        dfs(row, col - 1, grid);
     }
 }
