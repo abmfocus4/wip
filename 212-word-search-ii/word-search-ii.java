d780068f92749b22ee8896bbb28e40dd136693c8
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-12 14:32:27 -0400

    Time: 333 ms (41.24%) | Memory: 45 MB (49.06%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,65 +1,71 @@
-// Time Comp Analysis: https://leetcode.com/problems/word-search-ii/solutions/59780/java-15ms-easiest-solution-100-00/comments/161749
-// Code Ref: https://leetcode.com/problems/word-search-ii/solutions/156559/Java-(40ms)-Solution-with-Complexity-Analysis/
-// Code Ref: https://leetcode.com/problems/word-search-ii/solutions/59780/java-15ms-easiest-solution-100-00/
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
-    List<String> res = new ArrayList<>();
-    TrieNode root = buildTrie(words); // build prefix tree
-    for (int i = 0; i < board.length; i++) {
-        for (int j = 0; j < board[0].length; j++) { // start dfs at each location
-            dfs(board, i, j, root, res);
+        int m = board.length;
+        int n = board[0].length;
+        List<String> res = new ArrayList();
+
+        Trie root = new Trie(words);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, root, res);
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, Trie cur, List<String> res) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#' || cur.children.containsKey(board[i][j]) == false) {
+            return;
+        }
+
+        char c = board[i][j];
+        cur = cur.children.get(c);
+        if (cur.isWord && cur.word != null) {
+            res.add(cur.word);
+            cur.isWord = false;
+            cur.word = null;
+        }
+
+        board[i][j] = '#';
+        dfs(board, i + 1, j, cur, res);
+        dfs(board, i, j + 1, cur, res);
+        dfs(board, i, j - 1, cur, res);
+        dfs(board, i - 1, j, cur, res);
+        board[i][j] = c;
+    }
+
+    class Trie {
+        boolean isWord;
+        String word;
+        HashMap<Character, Trie> children;
+
+        Trie() {
+            this.isWord = false;
+            this.word = null;
+            this.children = new HashMap();
+        }
+
+        Trie(String[] words) {
+            this.isWord = false;
+            this.word = null;
+            this.children = new HashMap();
+            buildTree(this, words);
+        }
+
+        void buildTree(Trie root, String[] words) {
+            for (String word : words) {
+                Trie cur = root;
+                for (char c : word.toCharArray()) {
+                    if (cur.children.containsKey(c) == false) {
+                        cur.children.put(c, new Trie());
+                    }
+                    cur = cur.children.get(c);
+                }
+                cur.isWord = true;
+                cur.word = word;
+            }
         }
     }
-    return res;
-}
-
-public void dfs(char[][] board, int i, int j, TrieNode p, List<String> res) {
-    // base case
-    // return if out of bounds, if visited and if current cell is not a character in the trie
-    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == '#' || p.children.get(board[i][j]) == null) {
-        return;
-    }
-
-    char c = board[i][j];
-    p = p.children.get(c);
-    if (p.word != null && p.isWord == true) {   // found one word
-        res.add(p.word);
-        p.word = null; // de-duplicate
-        p.isWord = false; // de-duplicate
-    }
-
-    board[i][j] = '#'; // mark as visited alternatively use separate boolean[][] visited 2d arr
-    dfs(board, i - 1, j ,p, res); 
-    dfs(board, i, j - 1, p, res);
-    dfs(board, i + 1, j, p, res); 
-    dfs(board, i, j + 1, p, res); 
-    board[i][j] = c; // backtrack
-}
-
-// The trie is represented by a root node, not a Trie object
-private TrieNode buildTrie(String[] words) {
-    TrieNode root = new TrieNode();
-    for (String word : words) {
-        TrieNode curr = root;
-        for (char c : word.toCharArray()) {
-            if (!curr.children.containsKey(c))
-                curr.children.put(c, new TrieNode());
-            curr = curr.children.get(c);
-        }
-        curr.isWord = true;
-        curr.word = word; // Store a word at the leaf node
-    }
-    return root;
-}
-
-private class TrieNode {
-    boolean isWord; // this.word is null if isEndOfWord is false
-    String word; // Store the word so that no StringBuilder is needed to build the word char by char
-    Map<Character, TrieNode> children;
-    TrieNode() {
-        this.children = new HashMap<>();
-        this.isWord = false;
-        this.word = null; // de-duplicate
-    }
-}
 }
