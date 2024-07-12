5e00d19702e10872f5f101d55faaa873a5203c13
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-11 23:02:47 -0400

    Time: 27 ms (76.74%) | Memory: 57.4 MB (64.76%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,14 +1,63 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue();
-
-        for (int num : nums) {
-            pq.add(num);
-            if (pq.size() > k) {
-                pq.poll();
+    // https://www.youtube.com/watch?v=QFoNfrP7knk&ab_channel=codestorywithMIK
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+    
+    public int partition_algo(int[] nums, int L, int R) {
+        
+        int P = nums[L];
+        int i = L+1; //0
+        int j = R; //0
+        
+        while(i <= j) {
+            
+            if(nums[i] < P && nums[j] > P) {
+                swap(nums, i, j);
+                i++;
+                j--;
             }
+            
+            if(nums[i] >= P) {
+                i++;
+            }
+            
+            if(nums[j] <= P) {
+                j--;
+            }
+            
         }
-
-        return pq.poll();
+        
+        swap(nums, L, j);
+        return j; //P is at jth index
+    }
+    
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        
+        int L = 0;
+        int R = n-1;
+        
+        int pivot_idx = 0;
+        
+        //kth largest pivot element - nums[k-1] (descending order me partition karenge)
+        
+        while(true) {
+            
+            pivot_idx = partition_algo(nums, L, R);
+            
+            if(pivot_idx == k-1) {
+                break;
+            } else if(pivot_idx > k-1) { //2nd larget , 4th larget
+                R = pivot_idx - 1;
+            } else {
+                L = pivot_idx + 1;
+            }
+            
+        }
+        
+        return nums[pivot_idx];
     }
 }
