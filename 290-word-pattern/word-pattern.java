774a57f94efeaf0d75b64a17d938432027ef5b8f
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-13 11:41:44 -0400

    Time: 1 ms (83.14%) | Memory: 41.4 MB (21.82%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,13 +1,28 @@
-// https://leetcode.com/problems/word-pattern/solutions/73402/8-lines-simple-java
 class Solution {
-    public boolean wordPattern(String pattern, String str) {
-    String[] words = str.split(" ");
-    if (words.length != pattern.length())
-        return false;
-    Map index = new HashMap();
-    for (Integer i=0; i<words.length; ++i)
-        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
-            return false;
-    return true;
-}
+    public boolean wordPattern(String pattern, String str) {        
+        String[] words = str.split(" ");
+        
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> letterToWord = new HashMap<>(words.length);
+        Map<String, Character> wordToLetter = new HashMap<>(words.length);
+
+        for (int i = 0; i < words.length; ++i) {
+            char letter = pattern.charAt(i);
+            String word = words[i];
+
+            // case 1: one letter maps to different word
+            if (letterToWord.containsKey(letter) && !letterToWord.get(letter).equals(word))
+                return false;
+
+            // case 2: one word maps to different letter
+            if (wordToLetter.containsKey(word) && wordToLetter.get(word) != letter)
+                return false;
+
+            letterToWord.put(letter, word);
+            wordToLetter.put(word, letter);
+        }
+
+        return true;
+    }
 }
