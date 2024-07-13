b6b01551b1ed5efe5fdfa01c088b7b895753051d
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-13 18:33:01 -0400

    Time: 0 ms (100.00%) | Memory: 44.2 MB (69.11%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -10,12 +10,12 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null) return head;
-
+        if (head == null) {
+            return null;
+        }
         ListNode odd = head;
         ListNode even = head.next;
         ListNode evenHead = even;
-
         while (even != null && even.next != null) {
             odd.next = odd.next.next;
             even.next = even.next.next;
