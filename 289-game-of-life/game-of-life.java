6d8ac5dff00582443e635d0d8fa2e11f87bbaaac
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-13 12:01:30 -0400

    Time: 0 ms (100.00%) | Memory: 41.6 MB (44.16%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,55 +1,58 @@
-// https://www.youtube.com/watch?v=fei4bJQdBUQ&ab_channel=NeetCode
 class Solution {
-    // 1 -> 0 : < two 1s as neighbours // UPDATE
-    // 1 -> 1 : two or three 1s as neighbours
-    // 1 -> 0 : > three 1s as neighbours // UPDATE
-    // 0 -> 1 : three 1s as neighbours // UPDATE
-    int liveToDead = 2;
-    int deadToLive = 3;
+    int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 1 }, { -1, -1 }, { 1, -1 }, { -1, 1 } };
 
-    int m, n;
-    
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}}; // 9 neighbours
     private int getLiveNeighbours(int[][] board, int i, int j) {
-        int liveNeighbours = 0;
+        int count = 0;
         for (int[] dir : dirs) {
-            int newRow = i + dir[0];
-            int newCol = j + dir[1];
-            if (newRow >= 0 && newCol >= 0 && newRow < m && newCol < n) {
-                if (board[newRow][newCol] == 1 || board[newRow][newCol] == liveToDead) {
-                    // previous neighbours that are 1 before update
-                    liveNeighbours++;
-                }
+            int neighRow = i + dir[0];
+            int neighCol = j + dir[1];
+
+            if (neighRow < 0 || neighCol < 0 || neighRow >= board.length || neighCol >= board[0].length) {
+                continue;
+            }
+
+            int neighCell = board[neighRow][neighCol];
+
+            if (neighCell == 1 || neighCell == 3) {
+                count++;
             }
         }
-        return liveNeighbours;
+
+        return count;
+
     }
+
     public void gameOfLife(int[][] board) {
-        m = board.length;
-        n = board[0].length;
+        // live cell, live neighbours < 2; dies == 3
+        // live cell; live neighbours = 3/4; lives
+        // live cell; live neighbours > 3, dies == 3
+        // dead cell, live neighbours == 3; lives == 4
+
+        int m = board.length;
+        int n = board[0].length;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int curCell = board[i][j];
+                int cell = board[i][j];
                 int liveNeighbours = getLiveNeighbours(board, i, j);
-                if (board[i][j] == 0 && liveNeighbours == 3) {
-                    board[i][j] = deadToLive;
-                } else if (board[i][j] == 1) {
-                    if (liveNeighbours < 2 || liveNeighbours > 3) {
-                        board[i][j] = liveToDead;
-                    }
+                if (cell == 1 && (liveNeighbours < 2 || liveNeighbours > 3)) {
+                    board[i][j] = 3;
+                } else if (cell == 0 && liveNeighbours == 3) {
+                    board[i][j] = 4;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (board[i][j] == deadToLive) {
-                    board[i][j] = 1;
-                } else if (board[i][j] == liveToDead) {
+                if (board[i][j] == 3) {
                     board[i][j] = 0;
+                } else if (board[i][j] == 4) {
+                    board[i][j] = 1;
                 }
             }
         }
+
+        return;
     }
 }
