0ecc8a3c4ea2ba0bdfca9bb54b7e09e32bfe3ca8
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-15 13:28:48 -0400

    Time: 2 ms (100.00%) | Memory: 45.2 MB (28.09%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -1,23 +1,46 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        while(root != null && root.val != val) {
-            root = (val < root.val) ? root.left : root.right;
+        TreeNode cur = root;
+        TreeNode res = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                if (cur.val == val) { // process node
+                    res = cur;
+                }
+                cur = cur.right;
+            } else {
+                TreeNode prev = cur.left;
+                while (prev.right != null && prev.right != cur) {
+                    prev = prev.right;
+                }
+
+                if (prev.right == null) {
+                    prev.right = cur;
+                    cur = cur.left;
+                } else {
+                    prev.right = null;
+                    if (cur.val == val) { // process node
+                        res = cur;
+                    }
+                    cur = cur.right;
+                }
+            }
         }
-        return root;
+        return res;
     }
 }
