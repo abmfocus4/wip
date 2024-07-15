934df664a784505aae7b3da45265adfe56d719f4
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-15 14:31:04 -0400

    Time: 0 ms (100.00%) | Memory: 41.2 MB (25.84%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
@@ -1,37 +1,67 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    Integer prev = null;
-    int min = Integer.MAX_VALUE;
-    public int minDiffInBST(TreeNode root) {
-        inorder(root);
-        return min;
+    class MorrisIterator implements Iterator<Integer> {
+        private TreeNode current;
+
+        public MorrisIterator(TreeNode root) {
+            current = root;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        public Integer next() {
+            Integer val = null;
+            while (current != null) {
+                if (current.left == null) {
+                    val = current.val;
+                    current = current.right;
+                    break;
+                } else {
+                    TreeNode pre = current.left;
+                    while (pre.right != null && pre.right != current) {
+                        pre = pre.right;
+                    }
+                    if (pre.right == null) {
+                        pre.right = current;
+                        current = current.left;
+                    } else {
+                        pre.right = null;
+                        val = current.val;
+                        current = current.right;
+                        break;
+                    }
+                }
+            }
+            return val;
+        }
     }
 
-    private void inorder(TreeNode root) {
-        if (root == null) {
-            return;
+    public int minDiffInBST(TreeNode root) {
+        MorrisIterator mi = new MorrisIterator(root);
+        Integer prev = mi.next();
+        int minDiff = Integer.MAX_VALUE;
+        while (mi.hasNext()) {
+            int curr = mi.next();
+            minDiff = Math.min(minDiff, (curr - prev));
+            prev = curr;
         }
+        return minDiff;
 
-        inorder(root.left);
-        if (prev != null) {
-            min = Math.min(min, root.val - prev);
-        }
-        
-        prev = root.val;
-        inorder(root.right);
     }
 }
