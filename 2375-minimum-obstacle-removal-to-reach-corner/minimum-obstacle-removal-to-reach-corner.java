d571106b363c11ff23d18020ca9a51a596d00f04
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-16 14:56:44 -0400

    Time: 48 ms (92.68%) | Memory: 82.1 MB (26.79%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.java
@@ -1,39 +1,41 @@
+// 0-1 bfs using dq
+// add 0s to first
+// add 1s to last
+// since only two weights b/n nodes, don't need logN overhead of pq
 class Solution {
-    int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    int[][] dirs = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
+
     public int minimumObstacles(int[][] grid) {
-        // djikstra
-        // i, j, cost ()
-        // grid[i][j] = -2 means visited
+        int m = grid.length, n = grid[0].length;
 
-        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((u, v) -> Integer.compare(u[2], v[2]));
-        pq.add(new int[] {0, 0, 0});
+        int[][] dist = new int[m][n]; // obstacles needed to remove to get to m-1, n-1
 
-        int m = grid.length;
-        int n = grid[0].length;
+        for (int[] di : dist) {
+            Arrays.fill(di, Integer.MAX_VALUE);
+        }
 
-        while (pq.isEmpty() == false) {
-            int[] cur = pq.poll();
+        Deque<int[]> dq = new ArrayDeque<>();
+        dist[0][0] = 0;
+        dq.offer(new int[] { 0, 0, 0 });
+
+        while (!dq.isEmpty()) {
+            int[] cur = dq.poll();
             int curRow = cur[0], curCol = cur[1], curCost = cur[2];
-
-            if (curRow == m - 1 && curCol == n - 1) {
-                return curCost;
-            }
-
-            if (curRow < 0 || curCol < 0 || curRow >= m || curCol >= n || grid[curRow][curCol] == -2) {
-                continue;
-            }
-
-            int cell = grid[curRow][curCol];
-            grid[curRow][curCol] = -2;
-
             for (int[] dir : dirs) {
                 int newRow = curRow + dir[0];
                 int newCol = curCol + dir[1];
-                int newCost = curCost + (cell == 1 ? 1 : 0);
-                pq.add(new int[] {newRow, newCol, newCost}); 
+                // add to q only valid coordinates
+                if (0 <= newRow && newRow < m && 0 <= newCol && newCol < n && dist[newRow][newCol] == Integer.MAX_VALUE) {
+                    int newCost = curCost + (grid[newRow][newCol] == 1 ? 1 : 0);
+                    if (grid[newRow][newCol] == 1) {
+                        dq.addLast(new int[] { newRow, newCol, newCost }); // larger cost add last
+                    } else {
+                        dq.addFirst(new int[] { newRow, newCol, newCost }); // smaller cost add first
+                    }
+                    dist[newRow][newCol] = curCost;
+                }
             }
         }
-
-        return Integer.MAX_VALUE;
+        return dist[m - 1][n - 1];
     }
 }
