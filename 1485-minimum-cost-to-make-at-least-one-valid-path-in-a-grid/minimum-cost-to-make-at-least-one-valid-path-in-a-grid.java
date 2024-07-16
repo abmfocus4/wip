ac9e9b05d1e8561135529ff70ffe2ac707c9e704
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-16 12:26:36 -0400

    Time: 54 ms (16.89%) | Memory: 45.5 MB (13.33%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,47 +1,36 @@
-// https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/solutions/524886/java-c-python-bfs-and-dfs/comments/663095
 class Solution {
-    int rows, cols;
-    int[][] dirs = new int[][]{{0,1}, {0,-1},{1,0},{-1,0}};
+    int[][] steps = new int[][] {
+            { 0, 1 },
+            { 0, -1 },
+            { 1, 0 },
+            { -1, 0 }
+    };
+
     public int minCost(int[][] grid) {
-        this.rows = grid.length;
-        this.cols = grid[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((u, v) -> Integer.compare(u[2], v[2]));
+        pq.add(new int[] { 0, 0, 0 });
+        while (!pq.isEmpty()) {
+            int r = pq.peek()[0];
+            int c = pq.peek()[1];
+            int cost = pq.peek()[2];
+            pq.poll();
 
-        Queue<int[]> q = new LinkedList();
-        boolean[][] visited = new boolean[rows][cols];
-        moveTillYouCant(0, 0, q, grid, visited);
+            if (r + 1 == grid.length && c + 1 == grid[0].length)
+                return cost;
 
-        int cost = 0;
-        while (q.isEmpty() == false) {
-            int levelSize = q.size();
-            for (int i = 0; i < levelSize; i++) {
-                int[] curCell = q.poll();
-                int curCellRow = curCell[0];
-                int curCellCol = curCell[1];
+            if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == 0)
+                continue;
+            int sign = grid[r][c];
+            grid[r][c] = 0;
 
-                if (curCellRow == rows - 1 && curCellCol == cols - 1) {
-                    return cost;
-                }
-
-                // try changing direction of current cell and check
-                for (int[] dir : dirs) {
-                    moveTillYouCant(curCellRow + dir[0], curCellCol + dir[1], q, grid, visited);
-                }
-
+            for (int idx = 0; idx < steps.length; ++idx) {
+                int nr = r + steps[idx][0];
+                int nc = c + steps[idx][1];
+                int ncost = cost + (sign == idx + 1 ? 0 : 1);
+                pq.add(new int[] { nr, nc, ncost });
             }
-
-            cost++;
         }
-
-        return -1;
-    }
-
-    private void moveTillYouCant(int row, int col, Queue<int[]> q, int[][]grid, boolean[][]visited) {
-        while (row >= 0 && row < rows && col >= 0 && col < cols && visited[row][col] == false) {
-            q.add(new int[] {row, col});
-            visited[row][col] = true;
-            int[] dir = dirs[(grid[row][col] - 1) % 4];
-            row += dir[0];
-            col += dir[1];
-        }
+        // ~ unreachable
+        return Integer.MAX_VALUE;
     }
 }
