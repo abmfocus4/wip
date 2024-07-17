8738952aeb507cc4924556d0cd7852e419f1d995
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-17 15:10:25 -0400

    Time: 249 ms (5.78%) | Memory: 79 MB (28.65%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,11 +1,13 @@
 class Solution {
+    int minReorder;
     public int minReorder(int n, int[][] connections) {
         // create graph
         // assign neighbour if connections present
         // opp direction neighbour with negative sign
 
-        // bfs
-        // use q and visited to traverse the tree
+        // dfs
+        // start from 0
+        // visit all nodes
 
         Map<Integer, List<Integer>> graph = new HashMap();
 
@@ -18,30 +20,26 @@ class Solution {
             graph.get(end).add(-start); // child points to parent
         }
 
-        Queue<Integer> q = new LinkedList();
-        q.add(0);
-
-        boolean[] visited = new boolean[n];
-        Arrays.fill(visited, false);
-        visited[0] = true;
-
-
-        int minReorder = 0;
-        while (q.isEmpty() == false) {
-            int curNode = q.poll();
-            for (int neighbour : graph.get(curNode)) {
-                int neighbourNode = Math.abs(neighbour);
-                if (visited[neighbourNode] == true) {
-                    continue;
-                }
-                visited[neighbourNode] = true;
-                q.add(neighbourNode);
-                if (neighbour > 0) {
-                    minReorder++;
-                } 
-            }
-        }
-
+        this.minReorder = 0;
+        boolean[] visited = new boolean[n]; // 1 started processing, // 2 finished processing // -1 did not visit
+        visited[0] = true; // mark as visited here
+        dfs(0, visited, graph);
         return minReorder;
+    }
+
+    private void dfs(int node, boolean[] visited, Map<Integer, List<Integer>> graph) {
+        int curNode = Math.abs(node);
+        for (int neighbour : graph.get(curNode)) {
+            int neighbourNode = Math.abs(neighbour);
+            if (visited[neighbourNode] == true) {
+                continue;
+            }
+            visited[neighbourNode] = true; // mark as visited here
+            if (neighbour > 0) {
+                System.out.println(neighbour);
+                minReorder++;
+            }
+            dfs(neighbour, visited, graph);
+        }
     }
 }
