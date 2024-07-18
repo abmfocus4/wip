e44403b4cb8669af6ed0ee7a610e89003bca85ee
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-18 15:02:15 -0400

    Time: 1 ms (91.61%) | Memory: 48.3 MB (6.45%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,63 +1,34 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        // if i,j == 1 then isConnected. if you find a node that is not visited (is not previously connected) the run bfs using that node as starting point
+        // at the end all the nodes should be visited
         int n = isConnected.length;
-        UnionFind uf = new UnionFind(n);
+        boolean[] visited = new boolean[n];
+        Arrays.fill(visited, false);
+
+        int provinces = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1)
-                uf.union(i, j);
+            if (visited[i] == false) {
+                provinces++;
+                bfs(i, isConnected, visited);
             }
         }
-        return uf.numComponents;
+
+        return provinces;
     }
 
-    class UnionFind {
-        int[] parents;
-        int[] size;
-        int numComponents;
-
-        public UnionFind(int n) {
-            this.parents = new int[n];
-            this.size = new int[n];
-            this.numComponents = n;
-            for (int i = 0; i < n; i++) {
-                parents[i] = i;
-                size[i] = 1;
+    private void bfs(int node, int[][] isConnected, boolean[] visited) {
+        Queue<Integer> q = new LinkedList();
+        q.add(node);
+        while (q.isEmpty() == false) {
+            int curNode = q.poll();
+            // visit all neighbours and add them to q for traversal
+            for (int i = 0; i < isConnected.length; i++) {
+                if (isConnected[curNode][i] == 1 && visited[i] == false) {
+                    q.add(i);
+                    visited[i] = true;
+                }
             }
-        }
-
-        public int find(int cur) {
-            int root = cur;
-            while (parents[root] != root) {
-                root = parents[root];
-            }
-
-            while (cur != root) {
-                int curParent = parents[cur];
-                parents[cur] = root;
-                cur = curParent;
-            }
-
-            return root;
-        }
-
-        public void union(int node1, int node2) {
-            int node1Parent = find(node1);
-            int node2Parent = find(node2);
-
-            if (node1Parent == node2Parent) {
-                return;
-            }
-            
-            if (size[node1Parent] > size[node2Parent]) {
-                parents[node2Parent] = node1Parent;
-                size[node1Parent] += size[node2Parent];
-            } else {
-                parents[node1Parent] = node2Parent;
-                size[node2Parent] += size[node1Parent];
-            }
-
-            numComponents--;
         }
     }
 }
