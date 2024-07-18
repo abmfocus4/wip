2676b66cf05978183228adcc40efe76271bb2239
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-18 16:39:40 -0400

    Time: 33 ms (33.28%) | Memory: 55.3 MB (43.97%) - LeetSync

--- 359-logger-rate-limiter/logger-rate-limiter.java
+++ 359-logger-rate-limiter/logger-rate-limiter.java
@@ -1,26 +1,43 @@
+// explanation: https://leetcode.com/problems/logger-rate-limiter/solutions/365306/Simple-Two-HashMap-Solution-with-O(1)-time-and-little-memory/comments/903923
 class Logger {
-  private HashMap<String, Integer> msgDict;
-
-  /** Initialize your data structure here. */
-  public Logger() {
-    msgDict = new HashMap<String, Integer>();
-  }
-
-  /**
-   * Returns true if the message should be printed in the given timestamp, otherwise returns false.
-   */
-  public boolean shouldPrintMessage(int timestamp, String message) {
-
-    if (!this.msgDict.containsKey(message)) {
-      this.msgDict.put(message, timestamp);
-      return true;
+    Map<String, Integer> oldMap;
+    Map<String, Integer> latestMap;
+    int recentMsgTime;
+    public Logger() {
+        this.oldMap = new HashMap();
+        this.latestMap = new HashMap();
+        this.recentMsgTime = Integer.MIN_VALUE;
     }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (recentMsgTime <= timestamp - 10) {
+            Map<String, Integer> t = oldMap; 
+            oldMap = latestMap;
+            latestMap = t;
+            latestMap.clear();
+            recentMsgTime = timestamp;
+        }
 
-    Integer oldTimestamp = this.msgDict.get(message);
-    if (oldTimestamp <= timestamp - 10) {
-      this.msgDict.put(message, timestamp);
-      return true;
-    } else
-      return false;
-  }
+        if (oldMap.containsKey(message)) {
+            int oldTimestamp = oldMap.get(message);
+            if (!(oldTimestamp <= timestamp - 10)) {
+                return false;
+            }  
+        }
+
+                        
+        if (latestMap.containsKey(message)) {
+            return false;
+        }
+
+
+        latestMap.put(message, timestamp);
+        return true;
+    }
 }
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
