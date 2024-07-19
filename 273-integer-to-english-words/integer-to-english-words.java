67eea1fb812591a2db27249e5f4d1a5a12a833bd
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-18 21:22:10 -0400

    Time: 1 ms (100.00%) | Memory: 41.7 MB (75.46%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.java
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -1,22 +1,40 @@
 class Solution {
-    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
-                    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+
+    private final String[] belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 
     public String numberToWords(int num) {
-        if (num == 0)
+        if (num == 0) {
             return "Zero";
-        
-        return helper(num).trim();
+        }
+        return helper(num);
     }
-    
+
     private String helper(int num) {
-        
-        if (num >= 1000000000) return (helper(num / 1000000000) + " Billion " + helper(num % 1000000000)).trim();
-        if (num >= 1000000) return (helper(num / 1000000) + " Million " + helper(num % 1000000)).trim();
-        if (num >= 1000) return (helper(num / 1000) + " Thousand " + helper(num % 1000)).trim();
-        if (num >= 100) return (helper(num / 100) + " Hundred " + helper((num % 100))).trim();
-        if (num >= 20) return (tens[num / 10] + " " + helper(num % 10)).trim();
-        return ones[num];
+        StringBuilder result = new StringBuilder();
+        // if less than 20, won't continue forward 
+        // that is how else if's work
+
+        // if less than 20, will just add the word
+        if (num < 20) {
+            result.append(belowTwenty[num]);
+
+            // if less than 100, will add the tens and below 20 together
+        } else if (num < 100) {
+            result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
+            // adds Hundred to the word and appends the rest and does it for everything below
+        } else if (num < 1000) {
+            result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+        } else if (num < 1000000) { // 6 zeroes
+            result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+        } else if (num < 1000000000) { // 9 zeroes
+            result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+        } else {
+            result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+        }
+
+        // trim elimates all leading whitespaces in the string 
+        return result.toString().trim();
     }
-}
+} 
