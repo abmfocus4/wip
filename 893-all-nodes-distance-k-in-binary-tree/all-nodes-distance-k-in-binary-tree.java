e1fb4b5716b5208e595cc084ce5ea3cb003da1d1
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-20 22:26:05 -0400

    Time: 13 ms (74.30%) | Memory: 42.9 MB (21.35%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,67 +1,55 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode(int x) { val = x; }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
  * }
  */
-//  https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/solutions/143798/1ms-beat-100-simple-java-dfs-with-without-hashmap-including-explanation
 class Solution {
-    Map<TreeNode, Integer> map = new HashMap<>();
-
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> res = new ArrayList<>();
-        find(root, target);
-        search(root, 0, K, res);
+    HashMap<TreeNode, List<TreeNode>> graph;
+    HashSet<TreeNode> visited;
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        this.graph = new HashMap();
+        buildGraph(root, null);
+        this.visited = new HashSet();
+        visited.add(target);
+        List<Integer> res = new ArrayList();
+        dfs(target, k, 0, res);
         return res;
     }
 
-	// store the distance between the ancestor of target node and the target node
-    private void find(TreeNode root, TreeNode target) {
-        if (root == null) {
+    private void dfs(TreeNode node, int k, int distance, List<Integer> res) {
+        if (distance == k) {
+            res.add(node.val);
             return;
         }
 
-        if (root == target) {
-            map.put(root, 0);
+        if (graph.get(node) == null || graph.get(node).isEmpty()) {
             return;
-        }
-
-        find(root.left, target);
-        if (map.containsKey(root.left)) {
-            map.put(root, map.get(root.left) + 1);
-            return;
-        }
-
-        find(root.right, target);
-        if (map.containsKey(root.right)) {
-            map.put(root, map.get(root.right) + 1);
-            return;
-        }
-        return;
-    }
-
-    public void search(TreeNode root, int dis, int K, List<Integer> res) {
-        if (root == null) {
-            return;
-        }
-
-        //if node is the ancestor of target node, 
-		//we use the stored value in hashmap instead of the value (parameter len) from its parent node.
-        if (map.containsKey(root)) {
-            dis = map.get(root);
-        }
-
-        if (dis == K) {
-            res.add(root.val);
         }
         
-        //if the distance from a node to target node is k, 
-		//the distance from its child to the target node is k + 1,
-		//unless the child node is closer to the target node which means the target node is in it's subtree.
-        search(root.left, dis + 1, K, res);
-        search(root.right, dis + 1, K, res);
+        for (TreeNode neigh : graph.get(node)) {
+            if (!visited.contains(neigh)) {
+                visited.add(neigh);
+                dfs(neigh, k, distance + 1, res);
+            }
+        }
+    }
+
+    private void buildGraph(TreeNode cur, TreeNode parent) {
+        if (cur != null && parent != null) {
+            graph.computeIfAbsent(cur, x -> new ArrayList()).add(parent);
+            graph.computeIfAbsent(parent, x -> new ArrayList()).add(cur);
+        }
+
+        if (cur.left != null) {
+            buildGraph(cur.left, cur);
+        }
+
+        if (cur.right != null) {
+            buildGraph(cur.right, cur);
+        }
     }
 }
