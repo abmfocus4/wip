3f0abd9293b96ce712439791caee26359861ddc4
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-20 23:42:29 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (61.76%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,27 @@
-// https://www.youtube.com/watch?v=7wcJXZoGKMI&list=TLPQMDcwNjIwMjSq9lKrG3yYNQ&index=4&ab_channel=codestorywithMIK
-
-// https://leetcode.com/problems/powx-n/solutions/1337794/java-c-simple-o-log-n-easy-faster-than-100-explained
 class Solution {
     public double myPow(double x, int n) {
-        return solve(x, n);
+        return binaryExp(x, (long)n);
     }
 
-    private double solve(double x, long n) {
-        if (n == 0) return 1;
+    private double binaryExp(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
 
         if (n < 0) {
-            return solve(1/x, -n);
-        } else {
-            if (n % 2 == 0) {
-                return solve(x*x, n/2); // n >>>= 1 same as n/2
-            } else { // (n & 1) if 0  even
-                return solve(x*x, (n-1)/2) * x;
-            }
+            x = 1.0/x;
+            n *= -1;
         }
+
+        double result = 1;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                result *= x;
+                n = n-1;
+            }
+            x = x*x;
+            n = n/2;
+        }
+        return result;
     }
 }
