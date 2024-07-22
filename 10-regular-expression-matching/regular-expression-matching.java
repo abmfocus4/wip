2799bdb3add2bdcb6682357c798c8be320dad636
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-22 16:14:16 -0400

    Time: 1 ms (100.00%) | Memory: 42 MB (56.09%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,12 +1,20 @@
 class Solution {
+    Boolean[][] memo;
     public boolean isMatch(String s, String p) {
         if (p == null ^ s == null) return false;
+        int m = s.length();
+        int n = p.length();
+        this.memo = new Boolean[m+1][n+1];
         return isMatch(0, 0, s, p);
     }
 
     private boolean isMatch(int i, int j, String s, String p) {
         if (j == p.length()) {
             return i == s.length();
+        }
+
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
 
         // is next char in p * - take or don't take
@@ -17,9 +25,9 @@ class Solution {
         if (j+1 < p.length() && p.charAt(j + 1) == '*') {
             boolean take = charMatched && isMatch(i+1, j, s, p);
             boolean noTake = isMatch(i, j+2, s, p);
-            return take || noTake;
+            return memo[i][j] = take || noTake;
         } else {
-            return charMatched && isMatch(i+1, j+1, s, p);
+            return memo[i][j] = charMatched && isMatch(i+1, j+1, s, p);
         }
     }
 }
