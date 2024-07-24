47229d3e18ebfbd980c95015e5427f64c0ba60ad
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-24 12:42:50 -0400

    Time: 1 ms (100.00%) | Memory: 42.1 MB (43.65%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,14 +1,15 @@
 class Solution {
     Boolean[][] memo;
     public boolean isMatch(String s, String p) {
-        if (p == null ^ s == null) return false;
-        int m = s.length();
-        int n = p.length();
-        this.memo = new Boolean[m+1][n+1];
-        return isMatch(0, 0, s, p);
+        // s[i] match with p[j]
+        // 1. char to char
+        // 2. char to .
+        // 3. char to .*/c*
+        this.memo = new Boolean[s.length() + 1][p.length() + 1];
+        return isMatch(s, p, 0, 0);
     }
 
-    private boolean isMatch(int i, int j, String s, String p) {
+    private boolean isMatch(String s, String p, int i, int j) {
         if (j == p.length()) {
             return i == s.length();
         }
@@ -17,17 +18,16 @@ class Solution {
             return memo[i][j];
         }
 
-        // is next char in p * - take or don't take
-        // is not, first char is matched
+        boolean matchesCurrentChar = (i < s.length()) && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
 
-        boolean charMatched = (i < s.length()) && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
-
-        if (j+1 < p.length() && p.charAt(j + 1) == '*') {
-            boolean take = charMatched && isMatch(i+1, j, s, p);
-            boolean noTake = isMatch(i, j+2, s, p);
-            return memo[i][j] = take || noTake;
+        // c* and .*
+        if (j + 1 < p.length() && p.charAt(j+1) == '*') {
+            boolean takeZero = isMatch(s, p, i, j+2);
+            boolean takeMultiple = matchesCurrentChar && isMatch(s, p, i+1, j);
+            return memo[i][j] = takeZero || takeMultiple;
         } else {
-            return memo[i][j] = charMatched && isMatch(i+1, j+1, s, p);
+            // char match
+            return memo[i][j] = matchesCurrentChar && isMatch(s, p, i+1, j+1);
         }
     }
 }
