cf7c10425db24d3d480d5fe22c71527a0ba2cfc9
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-29 22:10:53 -0400

    Time: 11 ms (68.07%) | Memory: 44.9 MB (51.34%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,35 +1,27 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        // create a freq map for p
-        // create a freq map for s substring
-        // iterate through s in a sliding window fashion
-        // update the s map
-        // when end - start + 1 == p.length:
-        // compare both maps
-        // if equals: add start index to list
-        // else: update freq map of s and start++
-
-
-        int[] pFreqMap = new int[26];
+        int[] pMap = new int[26];
         for (char c : p.toCharArray()) {
-            pFreqMap[c - 'a']++;
+            pMap[c - 'a']++;
         }
-        int pLen = p.length();
-        List<Integer> startIdxList = new ArrayList();
-
-        int[] sFreqMap = new int[26];
+        int[] sMap = new int[26];
         int start = 0;
-        for (int end = 0; end < s.length(); end++) {
-            sFreqMap[s.charAt(end) - 'a']++;
-            if (end - start + 1 == pLen) {
-                if (Arrays.equals(sFreqMap, pFreqMap)) {
-                    startIdxList.add(start);
-                }
-                sFreqMap[s.charAt(start) - 'a']--;
-                start++;
+        List<Integer> result = new ArrayList();
+        // compare arrays of smap and pmap when count == p.length()
+        // append to list
+        for (int end = 0; end < s.length(); end++) 
+        {
+            sMap[s.charAt(end) - 'a']++;
+            if (end - start + 1 == p.length()) {
+               if (Arrays.equals(sMap, pMap)) {
+                    result.add(start);
+               }
+               sMap[s.charAt(start)-'a']--;
+               start++;
             }
         }
 
-        return startIdxList;
+        return result;
     }
+
 }
