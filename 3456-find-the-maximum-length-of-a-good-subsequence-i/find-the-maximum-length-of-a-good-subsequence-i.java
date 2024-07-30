05091402e63be2d5bc172b9be0dc19d251012103
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-29 23:01:43 -0400

    Time: 1940 ms (5.01%) | Memory: 146.4 MB (5.07%) - LeetSync

--- 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3456-find-the-maximum-length-of-a-good-subsequence-i/find-the-maximum-length-of-a-good-subsequence-i.java
@@ -1,13 +1,16 @@
 class Solution {
+    Integer[][][] memo;
     public int maximumLength(int[] nums, int k) {
+        // max "good subsequence"
+        // prev idx, cur idx needed to maintain subsequence
+        // take or not take curIdx
+        // k - 1 when nums[i] != nums[i+1]
         int n = nums.length;
-        int curIdx = 0;
-        int prevIdx = -1;
-        Integer[][][] memo = new Integer[n][n+1][k+1];
-        return getMaximumLength(curIdx, prevIdx, k, nums, memo);
+        this.memo = new Integer[n][n+1][k+1];
+        return maximumLength(nums, 0, -1, k);
     }
 
-    private int getMaximumLength(int curIdx, int prevIdx, int k, int[] nums, Integer[][][] memo) {
+    private int maximumLength(int[] nums, int curIdx, int prevIdx, int k) {
         if (curIdx == nums.length) {
             return 0;
         }
@@ -16,21 +19,15 @@ class Solution {
             return memo[curIdx][prevIdx + 1][k];
         }
 
-        // take curIdx
         int take = 0;
-        // 1. nums[i] == nums[prevIdx]
-        // 2. nums[i] != nums[prevIdx] 
         if (prevIdx == -1 || nums[curIdx] == nums[prevIdx]) {
-            take = 1 + getMaximumLength(curIdx + 1, curIdx, k, nums, memo);
-        } else if (prevIdx == -1 || nums[curIdx] != nums[prevIdx] && k > 0) {
-            take = 1 + getMaximumLength(curIdx + 1, curIdx, k - 1, nums, memo);
+            take = 1 + maximumLength(nums, curIdx + 1, curIdx, k);
+        } else if (prevIdx == -1 || nums[curIdx] != nums[prevIdx] && k > 0){
+            take = 1 + maximumLength(nums, curIdx + 1, curIdx, k - 1);
         }
 
-        // not take curIdx
-        int notTake = getMaximumLength(curIdx + 1, prevIdx, k, nums, memo);
+        int notTake = maximumLength(nums, curIdx + 1, prevIdx, k);
 
         return memo[curIdx][prevIdx + 1][k] = Math.max(take, notTake);
     }
-
-
 }
