3ed42d7af3db1e03f00a813fe19545fc8e6472f3
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-31 22:11:34 -0400

    Time: 5 ms (55.35%) | Memory: 59.6 MB (38.68%) - LeetSync

--- 2530-minimize-maximum-of-array/minimize-maximum-of-array.java
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -1,10 +1,15 @@
+// understand qs : https://leetcode.com/problems/minimize-maximum-of-array/description/comments/1854113
+// https://www.youtube.com/watch?v=AeHMvcKuR0Y&ab_channel=NeetCodeIO
 class Solution {
-    public int minimizeArrayValue(int[] A) {
-        long sum = 0, res = 0;
-        for (int i = 0; i < A.length; ++i) {
-            sum += A[i];
-            res = Math.max(res, (sum + i) / (i + 1));
+    public int minimizeArrayValue(int[] nums) {
+        long res = nums[0];
+        long total = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            total += nums[i];
+            res = Math.max(res, (total+i)/(i+1)); // trick to calculate ceiling
         }
+
         return (int)res;
     }
 }
