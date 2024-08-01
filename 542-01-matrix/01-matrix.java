2ef9a4428274eb968dbbdbd71a308a7ce1eba91b
Meg Alapati <bmalapat@uwaterloo.ca>
2024-07-31 21:19:07 -0400

    Time: 14 ms (62.68%) | Memory: 47.4 MB (36.95%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,55 +1,48 @@
-class State {
-    int row;
-    int col;
-    int steps;
-    State(int row, int col, int steps) {
-        this.row = row;
-        this.col = col;
-        this.steps = steps;
-    }
-}
-
 class Solution {
-    int m;
-    int n;
-    int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    
+    int[][] dirs = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    class State {
+        int row;
+        int col;
+        int steps;
+
+        State(int row, int col, int steps) {
+            this.row = row;
+            this.col = col;
+            this.steps = steps;
+        }
+    }
     public int[][] updateMatrix(int[][] mat) {
-        m = mat.length;
-        n = mat[0].length;
-        
+        // start bfs from each 0
+        // increment step from each cur state
+        int m = mat.length;
+        int n = mat[0].length;
         int[][] result = new int[m][n];
         boolean[][] seen = new boolean[m][n];
-        Queue<State> q = new LinkedList<>();
-        
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                result[row][col] = mat[row][col];
-                if (mat[row][col] == 0) {
-                    q.add(new State(row, col, 0));
-                    seen[row][col] = true;
+        Queue<State> q = new LinkedList();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = mat[i][j];
+                if (mat[i][j] == 0) {
+                    q.add(new State(i, j, 0));
+                    seen[i][j] = true;
                 }
             }
         }
-        
-        while (!q.isEmpty()) {
-            State state = q.poll();
-            int row = state.row, col = state.col, steps = state.steps;
-            
-            for (int[] dir: dirs) {
-                int nextRow = row + dir[0], nextCol = col + dir[1];
-                if (valid(nextRow, nextCol) && !seen[nextRow][nextCol]) {
-                    seen[nextRow][nextCol] = true;
-                    q.add(new State(nextRow, nextCol, steps + 1));
-                    result[nextRow][nextCol] = steps + 1; // only gets updated for non zeros
+
+        while (q.isEmpty() == false) {
+            State cur = q.poll();
+            int curRow = cur.row, curCol = cur.col, curSteps = cur.steps;
+            for (int[] dir : dirs) {
+                int newRow = dir[0] + curRow, newCol = dir[1] + curCol;
+                if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && seen[newRow][newCol] == false) {
+                    seen[newRow][newCol] = true;
+                    result[newRow][newCol] = curSteps + 1;
+                    q.add(new State(newRow, newCol, curSteps + 1));
                 }
             }
         }
-        
+
         return result;
-    }
-    
-    public boolean valid(int row, int col) {
-        return 0 <= row && row < m && 0 <= col && col < n;
     }
 }
