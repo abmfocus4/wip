219176cc77760f013e95f683f4d520505cf4f54b
Meg Alapati <bmalapat@uwaterloo.ca>
2024-08-01 00:22:31 -0400

    Time: 7 ms (48.04%) | Memory: 44.1 MB (66.98%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,21 +1,52 @@
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] prices = new int[n];
-        Arrays.fill(prices, Integer.MAX_VALUE);
-        prices[src] = 0;
-        int[] tmp = Arrays.copyOf(prices, n);
+    class PQElement {
+        int price;
+        int dst;
+        int steps;
 
-        for (int i = 0; i < k + 1; i++) {
-            for (int[] flight : flights) {
-                int from = flight[0], to = flight[1], price = flight[2];
-                if (prices[from] == Integer.MAX_VALUE) continue;
-                if (prices[from] + price < tmp[to]) {
-                    tmp[to] = prices[from] + price;
-                }
-            }
-            prices = Arrays.copyOf(tmp, n); // copy tmp prices to prices
+        PQElement(int price, int dst, int steps) {
+            this.price = price;
+            this.dst = dst;
+            this.steps = steps;
+        }
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        // dijkstra
+        // keep track of stops in [] for each node from src
+        // pq to greedily search for lowest price edges
+        // keep track of steps in edges
+        // dst node as well
+
+        // create graph
+        Map<Integer, List<int[]>> graph = new HashMap();
+
+        for (int[] flight : flights) {
+            int u = flight[0], v = flight[1], price = flight[2];
+            graph.computeIfAbsent(u, x -> new ArrayList()).add(new int[] {v, price});
         }
 
-        return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        stops[src] = 0;
+
+        // price, dst, steps
+        PriorityQueue<PQElement> pq = new PriorityQueue<>((a,b) -> a.price - b.price);
+        pq.add(new PQElement(0, src, 0));
+
+        while (pq.isEmpty() == false) {
+            PQElement cur = pq.poll();
+            int curPrice = cur.price, curNode = cur.dst, curSteps = cur.steps;
+            if (stops[curNode] < curSteps || curSteps > k + 1) continue;
+            stops[curNode] = curSteps;
+            if (curNode == dst) return curPrice;
+            if (graph.containsKey(curNode) == false) continue;
+            for (int[] nei : graph.get(curNode)) {
+                int neiNode = nei[0];
+                int curNodeToNeiPrice = nei[1];
+                pq.add(new PQElement(curNodeToNeiPrice + curPrice, neiNode, curSteps + 1));
+            }
+        }
+
+        return -1;
     }
 }
