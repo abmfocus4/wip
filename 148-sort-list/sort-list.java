bd628b46a49eac47496fa3fbef5e96424746e674
Meg Alapati <bmalapat@uwaterloo.ca>
2024-08-02 13:43:17 -0400

    Time: 9 ms (92.37%) | Memory: 56.9 MB (35.48%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,54 +1,61 @@
-public class Solution {
-// https://www.youtube.com/watch?v=8ocB7a_c-Cc&list=TLPQMDEwNjIwMjSUjnvbX72tSg&index=24&ab_channel=takeUforward
-    private ListNode findMiddle(ListNode head) {
-        // step 1. cut the list to two halves
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        // split list in two
+        // sort each list
+        // merge sorted list
+
+        // nlogn and logn auxilary stack space
+        if (head == null || head.next == null) return head;
+
+        ListNode middleNode = getMiddleNode(head);
+        ListNode right = middleNode.next;
+        middleNode.next = null;
+        ListNode list1 = sortList(head);
+        ListNode list2 = sortList(right);
+        return mergeSortedList(list1, list2);
+    }
+
+    private ListNode getMiddleNode(ListNode head) {
         ListNode slow = head, fast = head.next;
-        
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
         return slow;
     }
-  
-  public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
-        
-    ListNode middle = findMiddle(head);
-    ListNode right = middle.next;
-    middle.next = null;
-    
-    // step 2. sort each half
-    ListNode leftHalf = sortList(head);
-    ListNode rightHalf = sortList(right);
-    
-    // step 3. mergeSortedLists l1 and l2
-    return mergeSortedLists(leftHalf, rightHalf);
-  }
-  
-  ListNode mergeSortedLists(ListNode l1, ListNode l2) {
-    ListNode dummy = new ListNode(0), cur = dummy;
-    
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        cur.next = l1;
-        l1 = l1.next;
-      } else {
-        cur.next = l2;
-        l2 = l2.next;
-      }
-      cur = cur.next;
-    }
-    
-    if (l1 != null)
-      cur.next = l1;
-    
-    if (l2 != null)
-      cur.next = l2;
-    
-    return dummy.next;
-  }
 
+    private ListNode mergeSortedList(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        if (l1 != null) {
+            cur.next = l1;
+        }
+
+        if (l2 != null) {
+            cur.next = l2;
+        }
+
+        return dummy.next;
+    }
 }
