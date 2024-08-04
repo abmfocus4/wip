8d8826fcaf503e3f68eeaf8b7d6c8b369aea8bc6
Meg Alapati <bmalapat@uwaterloo.ca>
2024-08-04 10:40:02 -0400

    Time: 3 ms (21.40%) | Memory: 41.8 MB (26.80%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,16 +1,47 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder sb = new StringBuilder();
+    class Trie {
+        Map<Character, Trie> children;
+        boolean isWord;
 
-        for (int i = 0; i < strs[0].length(); i++) {
-            for (int j = 1; j < strs.length; j++) {
-                if (i > strs[j].length() - 1 || strs[0].charAt(i) != strs[j].charAt(i)) {
-                    return sb.toString();
-                }
+        Trie() {
+            this.children = new HashMap();
+            this.isWord = false;
+        }
+    }
+
+    private void insert(String str, Trie root) {
+        Trie cur = root;
+        for (char c : str.toCharArray()) {
+            if (cur.children.containsKey(c) == false) {
+                cur.children.put(c, new Trie());
             }
-            sb.append(strs[0].charAt(i));
+
+            cur = cur.children.get(c);
+        }
+        cur.isWord = true;
+    }
+    public String longestCommonPrefix(String[] strs) {
+        // add all strings to trie tree
+        // traverse till keyset.size() == 1 and append key to string builder
+        Trie root = new Trie();
+        for (String str : strs) {
+            insert(str, root);
         }
 
-        return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        Trie cur = root;
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            if (cur.children.containsKey(c) && cur.children.size() == 1 && cur.isWord == false) {
+                sb.append(c);
+                char next = cur.children.keySet().iterator().next();
+                cur = cur.children.get(next);
+            } else {
+                return sb.toString();
+            }
+        }
+
+        return strs[0];
     }
 }
