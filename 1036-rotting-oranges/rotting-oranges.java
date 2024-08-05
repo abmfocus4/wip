5fb00fb36d3d48002c9c69975bad98b4c7b81418
Meg Alapati <bmalapat@uwaterloo.ca>
2024-08-05 16:42:18 -0400

    Time: 2 ms (72.30%) | Memory: 42 MB (90.64%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,58 +1,48 @@
-// https://leetcode.com/problems/rotting-oranges/solutions/238681/java-clean-bfs-solution-with-comments
-// classic bfs
 class Solution {
-    public static final int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-
+    int[][] dirs = {{1,0} , {0,-1}, {-1, 0}, {0, 1}};
     public int orangesRotting(int[][] grid) {
-        if (grid == null || grid.length == 0)
-            return 0;
+        int m = grid.length, n = grid[0].length;
 
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int fresh = 0;
+        int mins = 0;
 
-        Queue<int[]> queue = new LinkedList<>();
-        int count_fresh = 0;
-        // Put the position of all rotten oranges in queue
-        // cal num of fresh oranges
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        Queue<int[]> q = new LinkedList();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
-                    queue.offer(new int[] { i, j });
+                    q.add(new int[] {i, j});
                 } else if (grid[i][j] == 1) {
-                    count_fresh++;
+                    fresh++;
                 }
             }
         }
-
-        // if num of fresh oranges is zero --> return 0
-        if (count_fresh == 0)
+        
+        if (fresh == 0) {
             return 0;
+        }
 
-        int mins = 0;
-        // bfs starting from initially rotten oranges
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] point = queue.poll();
-                for (int dir[] : dirs) {
-                    int x = point[0] + dir[0];
-                    int y = point[1] + dir[1];
-
-                    // out of bounds
-                    // if fresh orange is present
-                    if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 1) {
-                        // put the new rotten orange at (x , y) in queue
-                        queue.add(new int[] { x, y });
-                        // mark the orange at (x , y) as rotten
+        mins = 1;
+        while (q.isEmpty() == false) {
+            int levelSize = q.size();
+            while(levelSize-- > 0) {
+                int[] coord = q.poll();
+                for (int[] dir : dirs) {
+                    int x = coord[0] + dir[0];
+                    int y = coord[1] + dir[1];
+                    if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 1) {
+                        q.add(new int[] {x, y});
                         grid[x][y] = 2;
-                        // decrease the mins of fresh oranges by 1
-                        count_fresh--;
+                        fresh--;
+                        if (fresh == 0) {
+                            return mins;
+                        }
                     }
                 }
             }
-            mins++; // level increase
+            mins++;
         }
-        // if all fresh oranges are removed then return result otherwise -1
-        return count_fresh == 0 ? mins - 1 : -1;
+
+        return -1;
     }
 }
