024955ee1f65af50fada97519b68872527d0b301
Meg Alapati <bmalapat@uwaterloo.ca>
2024-08-07 23:56:24 -0400

    Time: 8 ms (5.26%) | Memory: 44.4 MB (36.19%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,22 +15,17 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        // serialize root
-        // serialize subroot
-        // kmp compare
-
         return kmp(serialize(root), serialize(subRoot));
     }
 
-    public String serialize(TreeNode root) {
+    private String serialize(TreeNode root) {
         StringBuilder sb = new StringBuilder();
         preorder(root, sb);
         return sb.toString();
     }
-
-    String nullStr = "$";
+    String nullStr = "#";
     String delimit = ",";
-    public void preorder(TreeNode root, StringBuilder sb) {
+    private void preorder(TreeNode root, StringBuilder sb) {
         if (root == null) {
             sb.append(delimit).append(nullStr);
         } else {
@@ -38,27 +33,25 @@ class Solution {
             preorder(root.left, sb);
             preorder(root.right, sb);
         }
+        
     }
 
-    public boolean kmp(String word, String pattern) {
+    private boolean kmp(String word, String pattern) {
         int[] lps = getLPS(pattern);
-        // iterate through each char of word
-        int m = word.length();
-        int j = 0;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0, j = 0; i < word.length(); i++) {
             while (word.charAt(i) != pattern.charAt(j) && j > 0) j = lps[j-1];
             if (word.charAt(i) == pattern.charAt(j)) ++j;
-            if (j == pattern.length()) return true;
+            if (j == pattern.length())  return true;
         }
+
         return false;
     }
 
     private int[] getLPS(String pattern) {
         int n = pattern.length();
         int[] lps = new int[n];
-        int j = 0;
-        for (int i = 1; i < n; i++) {
-            while (pattern.charAt(i) != pattern.charAt(j) && j > 0) j = lps[j-1];
+        for (int i = 1, j = 0; i < n; i++) {
+            while(pattern.charAt(i) != pattern.charAt(j) && j > 0) j = lps[j-1];
             if (pattern.charAt(i) == pattern.charAt(j)) lps[i] = ++j;
         }
         return lps;
